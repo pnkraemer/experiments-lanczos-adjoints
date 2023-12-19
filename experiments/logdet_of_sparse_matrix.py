893e4615a17f2c02340656520acaca735854bb9c
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-19 11:07:31 +0100

    Implement slq_value_and_grad, which is the more reliable option

--- experiments/logdet_of_sparse_matrix.py
+++ experiments/logdet_of_sparse_matrix.py
@@ -6,18 +6,19 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from matfree import hutchinson, slq
 
-from matfree_extensions import bcoo_random_spd, integrand_slq_spd_custom_vjp
+from matfree_extensions import (bcoo_random_spd, integrand_slq_spd_custom_vjp,
+                                integrand_slq_spd_value_and_grad)
 
-n = 10  # nrows/ncols
+n = 100  # nrows/ncols
 nz = 20  # nonzeros
 seed = 1
-order = 9
+num_samples = 1000
 
 key = jax.random.PRNGKey(seed)
 key_mat, key_estim1, key_estim2 = jax.random.split(key, num=3)
 
 x_like = jnp.ones((n,))
-sampler = hutchinson.sampler_normal(x_like, num=100)
+sampler = hutchinson.sampler_normal(x_like, num=num_samples)
 
 
 def matvec(x, p):
@@ -33,27 +34,59 @@ def logdet(p):
     return jnp.linalg.slogdet(M)[1]
 
 
-print(logdet(M)[1].todense())
+grad_true = logdet(M)[1]
 
 
-# Approximation
-integrand = integrand_slq_spd_custom_vjp(jnp.log, order, matvec)
-estimate_approximate = hutchinson.hutchinson(integrand, sampler)
-estimate_approximate = jax.jit(
-    jax.experimental.sparse.value_and_grad(
-        estimate_approximate, allow_int=True, argnums=1
+def error(g):
+    return jnp.linalg.norm((g - grad_true.data))
+
+
+for order in 2.0 ** (jnp.arange(7)):
+    order = int(order)
+    # Approximation
+    integrand = integrand_slq_spd_value_and_grad(
+        jnp.log, order, matvec, grad_func=jax.experimental.sparse.grad
     )
-)
-fx, grad = estimate_approximate(key_estim1, M)
-print(fx, grad.todense())
+    estimate_approximate = hutchinson.hutchinson(integrand, sampler)
+    estimate_approximate = jax.jit(estimate_approximate)
+    fx, grad = estimate_approximate(key_estim1, M)
+    print(order, error(grad))
 
+print()
+for order in 2.0 ** (jnp.arange(7)):
+    order = int(order)
 
-# Reference
-integrand = slq.integrand_slq_spd(jnp.log, order, matvec)
-estimate_reference = hutchinson.hutchinson(integrand, sampler)
-estimate_reference = jax.jit(
-    jax.experimental.sparse.value_and_grad(
-        estimate_reference, allow_int=True, argnums=1
+    # Reference
+    integrand = slq.integrand_slq_spd(jnp.log, order, matvec)
+    estimate_reference = hutchinson.hutchinson(integrand, sampler)
+    estimate_reference = jax.jit(
+        jax.experimental.sparse.value_and_grad(
+            estimate_reference, allow_int=True, argnums=1
+        )
     )
-)
-print(estimate_reference(key_estim1, M)[1].todense())
+    _fx, grad = estimate_reference(key_estim1, M)
+    print(order, error(grad.data))
+
+#
+# vmin, vmax = -10, 10
+#
+# mosaic = [["truth", "backprop-slq", "backprop-naive"],
+#                               ["error-truth", "error-backprop-slq", "error-backprop-naive"]]
+# fig, ax = plt.subplot_mosaic(mosaic, sharex=True, sharey=True)
+#
+# ax["truth"].set_ylabel("Gradient")
+# ax["error-truth"].set_ylabel("Error (gradient)")
+#
+#
+# ax["truth"].spy(grad_true.todense())
+# ax["error-truth"].imshow(jnp.log10(1e-10 + jnp.abs((grad_true-grad_true).todense())), vmin=vmin, vmax=vmax)
+#
+# ax["backprop-slq"].spy(grad.todense())
+# ax["error-backprop-slq"].imshow(jnp.log10(1e-10 + jnp.abs((grad-grad_true).todense())), vmin=vmin, vmax=vmax)
+#
+#
+# ax["backprop-naive"].spy(grad.todense())
+# img = ax["error-backprop-naive"].imshow(jnp.log10(1e-10 + jnp.abs((grad-grad_true).todense())), vmin=vmin, vmax=vmax)
+#
+# plt.colorbar(img)
+# plt.show()
--- src/matfree_extensions.py
+++ src/matfree_extensions.py
@@ -6,6 +6,58 @@ from matfree import decomp
 from matfree.backend import func, linalg, tree_util
 
 
+def integrand_slq_spd_value_and_grad(matfun, order, matvec, /, grad_func):
+    def quadform(v0, *parameters):
+        v0_flat, v_unflatten = tree_util.ravel_pytree(v0)
+
+        def matvec_flat(v_flat):
+            v = v_unflatten(v_flat)
+            Av = matvec(v, *parameters)
+            flat, unflatten = tree_util.ravel_pytree(Av)
+            return flat
+
+        algorithm = decomp.lanczos_tridiag_full_reortho(order)
+        basis, tridiag = decomp.decompose_fori_loop(
+            v0_flat, matvec_flat, algorithm=algorithm
+        )
+        (diag, off_diag) = tridiag
+
+        # todo: once jax supports eigh_tridiagonal(eigvals_only=False),
+        #  use it here. Until then: an eigen-decomposition of size (order + 1)
+        #  does not hurt too much...
+        diag = linalg.diagonal_matrix(diag)
+        offdiag1 = linalg.diagonal_matrix(off_diag, -1)
+        offdiag2 = linalg.diagonal_matrix(off_diag, 1)
+        dense_matrix = diag + offdiag1 + offdiag2
+        eigvals, eigvecs = linalg.eigh(dense_matrix)
+
+        # Since Q orthogonal (orthonormal) to v0, Q v = Q[0],
+        # and therefore (Q v)^T f(D) (Qv) = Q[0] * f(diag) * Q[0]
+        (dim,) = v0_flat.shape
+
+        # Evaluate the matrix-function
+        fx_eigvals = func.vmap(matfun)(eigvals)
+        slqval = dim * linalg.vecdot(eigvecs[0, :], fx_eigvals * eigvecs[0, :])
+
+        # Evaluate the derivative
+        dfx_eigvals = func.vmap(func.jacfwd(matfun))(eigvals)
+        sol = eigvecs @ (dfx_eigvals * eigvecs[0, :].T)
+        w1, w2 = linalg.vector_norm(v0) * (basis.T @ sol), v0
+
+        @tree_util.partial_pytree
+        def matvec_flat_p(v_flat, *p):
+            v = v_unflatten(v_flat)
+            av = matvec(v, *p)
+            flat, unflatten = tree_util.ravel_pytree(av)
+            return flat
+
+        grad = grad_func(lambda *pa: matvec_flat_p(w2, *pa).T @ w1)(*parameters)
+
+        return slqval, grad.data
+
+    return quadform
+
+
 def integrand_slq_spd_custom_vjp(matfun, order, matvec, /):
     @jax.custom_vjp
     def quadform(v0, *parameters):
