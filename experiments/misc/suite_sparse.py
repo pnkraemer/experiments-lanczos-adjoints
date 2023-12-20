fe57c965a32f4accbf44026ce6c935e39ceff558
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-20 14:46:15 +0100

    Implement suite_sparse_download and suite_sparse_load

--- experiments/misc/suite_sparse.py
+++ experiments/misc/suite_sparse.py
@@ -1,6 +1,17 @@
 """Suite-sparse playground."""
 
 
+import matplotlib.pyplot as plt
+
 from matfree_extensions import test_util
 
-test_util.suite_sparse_download()
+PATH = "./data/matrices/"
+
+test_util.suite_sparse_download(path=PATH)
+M = test_util.suite_sparse_load("494_bus", path=PATH)
+
+fig, ax = plt.subplots(ncols=7)
+ax[0].spy(M.todense())
+for i, a in enumerate(ax[1:]):
+    a.spy(M.todense(), markersize=i + 1)
+plt.show()
