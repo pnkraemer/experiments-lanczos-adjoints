7038f2637307748913795501da5e3ce695dd3e5e
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-22 11:01:01 +0100

    Rename experiment_with_monte_carlo_jvp script

--- experiments/playground/experiment_with_monte_carlo_jvp.py
+++ experiments/playground/experiment_with_monte_carlo_jvp.py
@@ -29,29 +29,6 @@ def hutchinson_share(integrand_fun, /, sample_fun):
     return sample
 
 
-def hutchinson_indep(integrand_fun, /, sample_fun):
-    # todo: what is the derivative wrt the key?
-    # todo: do we HAVE to reuse the same monte-Carlo samples?
-    @functools.partial(jax.custom_jvp, nondiff_argnums=[0])
-    def sample(k, *parameters):
-        samples = sample_fun(k)
-        Qs = jax.vmap(lambda vec: integrand_fun(vec, *parameters))(samples)
-        return jax.tree_util.tree_map(lambda s: jnp.mean(s, axis=0), Qs)
-
-    def sample_jvp(k_primals, p_primals, p_tangents):
-        samples = sample_fun(k_primals)
-
-        def integrand_jvp(vec):
-            integrand_p = functools.partial(integrand_fun, vec)
-            return jax.jvp(integrand_p, p_primals, p_tangents)
-
-        Qs = jax.vmap(integrand_jvp)(samples)
-        return jax.tree_util.tree_map(lambda s: jnp.mean(s, axis=0), Qs)
-
-    sample.defjvp(sample_jvp)
-    return sample
-
-
 def f(x, p):
     return p * x**2
 
@@ -65,7 +42,6 @@ estimated_share, jvp_share = jax.jvp(
     functools.partial(estimate_share, key), [2.0], [1.0]
 )
 print("Primals (again)", estimated_share)
-print()
 print("JVP", jvp_share)
 print()
 
@@ -78,5 +54,5 @@ estimated_matfree, jvp_matfree = jax.jvp(
     functools.partial(estimate_matfree, key), [2.0], [1.0]
 )
 print("Primals (again)", estimated_matfree)
-print()
 print("JVP", jvp_matfree)
+print()
