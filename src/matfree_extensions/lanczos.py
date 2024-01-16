3f680a6361332b666a362ee46fc508c8b0756f1f
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-16 18:25:30 +0100

    Collect deprecated (old) API in a deprecated.py module

---
+++ src/matfree_extensions/lanczos.py
@@ -0,0 +1,84 @@
+"""Extensions for the Matfree package."""
+
+
+import jax
+import jax.flatten_util
+import jax.numpy as jnp
+from matfree import lanczos
+
+
+def integrand_spd_custom_vjp(matfun, order, matvec, /):
+    """Construct an integrand for SLQ for SPD matrices that comes with a custom VJP.
+
+    The custom VJP efficiently computes a single backward-pass (by reusing
+    the Lanczos decomposition from the forward pass), but does not admit
+    higher derivatives.
+    """
+
+    @jax.custom_vjp
+    def quadform(v0, *parameters):
+        return quadform_fwd(v0, *parameters)[0]
+        #
+        # This function shall only be meaningful inside a VJP,
+        # thus, we raise a:
+        #
+        raise RuntimeError
+
+    def quadform_fwd(v0, *parameters):
+        v0_flat, v_unflatten = jax.flatten_util.ravel_pytree(v0)
+        scale = jnp.linalg.norm(v0_flat)
+        v0_flat /= scale
+
+        @jax.tree_util.Partial
+        def matvec_flat(v_flat, *p):
+            v = v_unflatten(v_flat)
+            av = matvec(v, *p)
+            flat, unflatten = jax.flatten_util.ravel_pytree(av)
+            return flat
+
+        algorithm = lanczos.alg_tridiag_full_reortho(matvec_flat, order)
+        basis, (diag, off_diag) = algorithm(v0_flat, *parameters)
+
+        # todo: once jax supports eigh_tridiagonal(eigvals_only=False),
+        #  use it here. Until then: an eigen-decomposition of size (order + 1)
+        #  does not hurt too much...
+        diag = jnp.diag(diag)
+        offdiag1 = jnp.diag(off_diag, -1)
+        offdiag2 = jnp.diag(off_diag, 1)
+        dense_matrix = diag + offdiag1 + offdiag2
+
+        eigvals, eigvecs = jnp.linalg.eigh(dense_matrix)
+
+        # Since Q orthogonal (orthonormal) to v0, Q v = Q[0],
+        # and therefore (Q v)^T f(D) (Qv) = Q[0] * f(diag) * Q[0]
+        # Evaluate the matrix-function
+        fx_eigvals = jax.vmap(matfun)(eigvals)
+        slqval = scale**2 * jnp.dot(eigvecs[0, :], fx_eigvals * eigvecs[0, :])
+
+        # Evaluate the derivative
+        dfx_eigvals = jax.vmap(jax.jacfwd(matfun))(eigvals)
+        sol = eigvecs @ (dfx_eigvals * eigvecs[0, :].T)
+        w1, w2 = scale**2 * (basis.T @ sol), v0_flat
+
+        # Return both
+        cache = {
+            "matvec_flat": matvec_flat,
+            "w1": w1,
+            "w2": w2,
+            "parameters": parameters,
+        }
+        return slqval, cache
+
+    def quadform_bwd(cache, vjp_incoming):
+        matvec_flat = cache["matvec_flat"]
+        p = cache["parameters"]
+        w1, w2 = cache["w1"], cache["w2"]
+
+        fx, vjp = jax.vjp(lambda *pa: matvec_flat(w2, *pa).T @ w1, *p)
+
+        # todo: compute gradient wrt v?
+        return 0.0, *vjp(vjp_incoming)
+
+    quadform.defvjp(quadform_fwd, quadform_bwd)
+
+    return quadform
---
+++ tests/test_deprecated.py
@@ -0,0 +1,66 @@
+"""Tests for the extensions."""
+import jax
+from matfree import hutchinson, lanczos, test_util
+from matfree.backend import np, prng
+
+from matfree_extensions import deprecated
+from matfree_extensions import lanczos as lanczos_extensions
+
+
+def test_slq_spd_value_and_grad_matches_autodiff(n=10):
+    eigvals = np.arange(0.0, 1.0 + n, step=1.0) + 1.0
+    A = test_util.symmetric_matrix_from_eigenvalues(eigvals)
+
+    def matvec(x, p):
+        return p @ x
+
+    x_like = np.ones((len(A),))
+    sampler = hutchinson.sampler_rademacher(x_like, num=1_000)
+    order = n // 2
+
+    # Reference
+    integrand = lanczos.integrand_spd(np.log, order, matvec)
+    estimate = hutchinson.hutchinson(integrand, sampler)
+    key = prng.prng_key(seed=2)
+    slq_value, slq_gradient = (jax.value_and_grad(estimate, argnums=1))(key, A)
+
+    # Implementation
+    integrand = deprecated.integrand_spd_value_and_grad(np.log, order, matvec)
+    estimate = hutchinson.hutchinson(integrand, sampler)
+    slq_value_and_grad = (estimate)(key, A)
+
+    # Value should be extremely close
+    assert np.allclose(slq_value, slq_value_and_grad[0], rtol=1e-10, atol=0.0)
+
+    # Gradients tolerances are pretty tight
+    rtol, atol = 0.1, 0.1
+    assert np.allclose(slq_gradient, slq_value_and_grad[1], rtol=rtol, atol=atol)
+
+
+def test_slq_spd_custom_vjp_recursive_matches_slq_spd_custom_vjp(n=10):
+    """For full orders, recursive and custom VJPs should be identical."""
+    eigvals = np.arange(0.0, 1.0 + n, step=1.0) + 1.0
+    A = test_util.symmetric_matrix_from_eigenvalues(eigvals)
+
+    def matvec(x, p):
+        return p @ x
+
+    x_like = np.ones((len(A),))
+    sampler = hutchinson.sampler_normal(x_like, num=1)
+    order = n
+
+    reference2 = lanczos_extensions.integrand_spd_custom_vjp(np.log, order, matvec)
+    reference2 = hutchinson.hutchinson(reference2, sampler)
+    implementation = deprecated.integrand_spd_custom_vjp_recursive(
+        np.log, order, matvec
+    )
+    implementation = hutchinson.hutchinson(implementation, sampler)
+
+    key = prng.prng_key(seed=1)
+    value_ref, grad_ref = jax.jit(jax.value_and_grad(reference2, argnums=1))(key, A)
+    value_impl, grad_impl = jax.jit(jax.value_and_grad(implementation, argnums=1))(
+        key, A
+    )
+
+    assert np.allclose(value_ref, value_impl)
+    assert np.allclose(grad_ref, grad_impl)
