edde294cefe470841d2ea3ddaad1bc9ba633bfc0
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-22 12:11:51 +0100

    Demonstrate the symmetry of the Lanczos process

--- experiments/playground/demonstrate_symmetry_of_lanczos_process.py
+++ experiments/playground/demonstrate_symmetry_of_lanczos_process.py
@@ -1,10 +1,10 @@
-"""Implement the adjoint of the Lanczos process."""
+"""Demonstrate the symmetry of the Lanczos process."""
 import jax
 import jax.numpy as jnp
 from matfree import test_util
 
 
-def lanczos_init(matrix, vec):
+def lanczos_fwd_init(matrix, vec):
     """Initialize Lanczos' algorithm.
 
     Solve A x_{k} = a_k x_k + b_k x_{k+1}
@@ -14,11 +14,11 @@ def lanczos_init(matrix, vec):
     a = vec @ (matrix @ vec)
     r = matrix @ vec - a * vec
     b = jnp.linalg.norm(r)
-    x = r / jnp.linalg.norm(b)
+    x = r / b
     return (x, b), a
 
 
-def lanczos_step(matrix, vec, b, vec_previous):
+def lanczos_fwd_step(matrix, vec, b, vec_previous):
     """Apply Lanczos' recurrence.
 
     Solve
@@ -29,22 +29,40 @@ def lanczos_step(matrix, vec, b, vec_previous):
     a = vec @ (matrix @ vec)
     r = matrix @ vec - a * vec - b * vec_previous
     b = jnp.linalg.norm(r)
-    x = r / jnp.linalg.norm(b)
+    x = r / b
     return (x, b), a
 
 
+def lanczos_rev_step(matrix, vec, b, vec_next):
+    a = vec.T @ (matrix @ vec)
+    r = (matrix @ vec) - a * vec - b * vec_next
+    b = jnp.linalg.norm(r)
+    x = r / b
+    return (x, b), a
+
+
+# Set up a test-matrix
 eigvals = jnp.ones((50,), dtype=float) * 0.001
 eigvals_relevant = jnp.arange(1.0, 2.0, step=0.1)
 eigvals = eigvals.at[: len(eigvals_relevant)].set(eigvals_relevant)
 A = test_util.symmetric_matrix_from_eigenvalues(eigvals)
 
+# Set up an initial vector
 key = jax.random.PRNGKey(1)
 v0 = jax.random.normal(key, shape=jnp.shape(eigvals))
 v0 /= jnp.linalg.norm(v0)
 
-(v1, offdiag), diag = lanczos_init(A, v0)
 
-i, small_value = 0, jnp.sqrt(jnp.finfo(jnp.dtype(offdiag)).eps)
+# Initialize and apply the forward recursion
+i, small_value = 0, jnp.sqrt(jnp.finfo(jnp.dtype(v0)).eps)
+(v1, offdiag), diag = lanczos_fwd_init(A, v0)
 while (offdiag > small_value) and (i := (i + 1)) < len(eigvals):
-    ((v1, offdiag), diag), v0 = lanczos_step(A, v1, offdiag, v0), v1
+    ((v1, offdiag), diag), v0 = lanczos_fwd_step(A, v1, offdiag, v0), v1
     print(i, offdiag)
+
+# Go backwards:
+# Forward recursion, but change the roles of v0 and v1
+# while (offdiag > small_value) and (i := (i + 1)) < len(eigvals):
+for j in range(i - 1, 0, -1):
+    ((v0, offdiag), diag), v1 = lanczos_fwd_step(A, v0, offdiag, v1), v0
+    print(j, offdiag)
