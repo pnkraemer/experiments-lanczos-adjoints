Fix the gradient wrt v

experiments/playground/implement_adjoint_of_lanczos_process.py
@@ -89,12 +89,10 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
         dxs = jnp.concatenate((dxs, dx_last[None]))
         dbetas = jnp.concatenate((dbetas, dbeta_last[None]))
 
-        # print(jax.tree_util.tree_map(jnp.shape, cache))
-
         ((xs, alphas, betas), (x_last, beta_last)), *params = cache
-        # print(*params)
         xs = jnp.concatenate((xs, x_last[None]))
         betas = jnp.concatenate((betas, beta_last[None]))
+        print("dxk_all", dxs)
 
         k = len(alphas) - 1
         nu_k, lambda_k = _bwd_init(
@@ -107,15 +105,13 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
         )
         lambda_kplus = jnp.zeros_like(lambda_k)
         lambda_kplusplus, lambda_kplus = lambda_kplus, lambda_k
-
         # a-constraint
-        # assert jnp.allclose(lambda_kplus.T @ xs[k], dalphas[k])
+        assert jnp.allclose(lambda_kplus.T @ xs[k], dalphas[k])
 
         # b-constraint
-        # assert jnp.allclose(lambda_kplus.T @ xs[k + 1], dbetas[k])
+        assert jnp.allclose(lambda_kplus.T @ xs[k + 1], dbetas[k])
 
         dA = jnp.outer(lambda_kplus, xs[k])
-
         for k in range(len(alphas) - 1, 0, -1):
             nu_k, lambda_k = _bwd_step(
                 *params,
@@ -133,12 +129,12 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
                 x_Kminus=xs[k - 1],
             )
             # a-constraint
-            # assert jnp.allclose(lambda_kplus.T @ xs[k], dalphas[k])
+            assert jnp.allclose(lambda_kplus.T @ xs[k], dalphas[k])
 
             # b-constraint
-            # assert jnp.allclose(
-            #     lambda_kplusplus.T @ xs[k] + lambda_kplus.T @ xs[k + 1], dbetas[k]
-            # )
+            assert jnp.allclose(
+                lambda_kplusplus.T @ xs[k] + lambda_kplus.T @ xs[k + 1], dbetas[k]
+            )
 
             # Update
             dA += jnp.outer(lambda_kplus, xs[k])
@@ -146,7 +142,8 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
             lambda_kplusplus, lambda_kplus = lambda_kplus, lambda_k
 
         lambda_1 = (
-            betas[0] * lambda_kplusplus
+            -dxs[0]
+            + betas[0] * lambda_kplusplus
             - matvec(lambda_kplus, *params)
             + alphas[0] * lambda_kplus
             - nu_k * xs[1]
@@ -155,6 +152,7 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
         return dv, dA
 
     def _bwd_init(dx_Kplus, da_K, db_K, b_K, x_Kplus, x_K):
+        print("dxk", dx_Kplus)
         mu_K = db_K * b_K - x_Kplus.T @ dx_Kplus
         nu_K = da_K * b_K - x_K.T @ dx_Kplus
         lambda_Kplus = (dx_Kplus + mu_K * x_Kplus + nu_K * x_K) / b_K
@@ -175,6 +173,7 @@ def lanczos_fwd(matvec, *, custom_vjp: bool):
         x_K,
         x_Kminus,
     ):
+        print("dxk", dx_K)
         xi = (
             dx_K
             + matvec(lambda_kplus, *params)
@@ -221,7 +220,12 @@ def algorithm_flat(f):
     return jax.flatten_util.ravel_pytree(algorithm(*unflatten(f)))[0]
 
 
-print(flat)
+fx, vjp = jax.vjp(algorithm_flat, flat)
+
+e1 = jnp.eye(len(fx))[0]
+e1 = jnp.arange(1.0, len(fx) + 1.0)
+_ = vjp(e1)
+assert False
 print(algorithm_flat(flat))
 
 