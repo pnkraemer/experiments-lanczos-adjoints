739868604838e43ce1fc2d3cdb54a239515f505b
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-25 13:54:45 +0100

    Benchmark the runtimes

--- experiments/tridiag_custom_vjp/benchmark.py
+++ experiments/tridiag_custom_vjp/benchmark.py
@@ -9,8 +9,7 @@ import jax.numpy as jnp
 
 from matfree_extensions import lanczos
 
-krylov_order = 4
-n = 100
+n = 1_000
 seed = 1
 
 
@@ -23,36 +22,46 @@ vector = jax.random.normal(jax.random.PRNGKey(seed + 1), shape=(n,))
 # Flatten the inputs
 flat, unflatten = jax.flatten_util.ravel_pytree((vector, params))
 
+krylov_depth = 1
+while (krylov_depth := 2 * krylov_depth) < n:
+    print("Krylov-depth:", krylov_depth)
 
-# Construct an vector-to-vector decomposition function
-def decompose(f, *, custom_vjp):
-    algorithm = lanczos.tridiag(lambda s, p: p * s, krylov_order, custom_vjp=custom_vjp)
-    output = algorithm(*unflatten(f))
-    return jax.flatten_util.ravel_pytree(output)[0]
+    # Construct an vector-to-vector decomposition function
+    def decompose(f, *, custom_vjp):
+        algorithm = lanczos.tridiag(
+            lambda s, p: p * s, krylov_depth, custom_vjp=custom_vjp
+        )
+        output = algorithm(*unflatten(f))
+        return jax.flatten_util.ravel_pytree(output)[0]
 
+    # Construct the two implementations
+    reference = jax.jit(functools.partial(decompose, custom_vjp=False))
+    implementation = jax.jit(functools.partial(decompose, custom_vjp=True))
 
-# Construct the two implementations
-reference = jax.jit(functools.partial(decompose, custom_vjp=False))
-implementation = jax.jit(functools.partial(decompose, custom_vjp=True))
+    # Compute both VJPs
+    fx_ref, vjp_ref = jax.vjp(reference, flat)
+    fx_imp, vjp_imp = jax.vjp(implementation, flat)
+    # Assert that the forward-passes are identical
+    assert jnp.allclose(fx_ref, fx_imp)
 
-# Compute both VJPs
-fx_ref, vjp_ref = jax.vjp(reference, flat)
-fx_imp, vjp_imp = jax.vjp(implementation, flat)
-# Assert that the forward-passes are identical
-assert jnp.allclose(fx_ref, fx_imp)
+    # Assert that the VJPs into a bunch of random directions are identical
+    key = jax.random.PRNGKey(seed + 2)
+    dnu = jax.random.normal(key, shape=jnp.shape(reference(flat)))
 
-# Assert that the VJPs into a bunch of random directions are identical
-key = jax.random.PRNGKey(seed + 2)
-dnu = jax.random.normal(key, shape=jnp.shape(reference(flat)))
-assert jnp.allclose(*vjp_ref(dnu), *vjp_imp(dnu), atol=1e-4, rtol=1e-4)
+    # assert jnp.allclose(*vjp_ref(dnu), *vjp_imp(dnu), atol=1e-4, rtol=1e-4)
 
+    t0 = time.perf_counter()
+    for _ in range(2):
+        _ = vjp_ref(dnu)[0].block_until_ready()
+    t1 = time.perf_counter()
+    time_autodiff = (t1 - t0) / 2
+    print("Time (AutoDiff):", time_autodiff)
 
-t0 = time.perf_counter()
-_ = vjp_ref(dnu)[0].block_until_ready()
-t1 = time.perf_counter()
-print("Time (AutoDiff):", t1 - t0)
-
-t0 = time.perf_counter()
-_ = vjp_imp(dnu)[0].block_until_ready()
-t1 = time.perf_counter()
-print("Time (custom VJP):", t1 - t0)
+    t0 = time.perf_counter()
+    for _ in range(2):
+        _ = vjp_imp(dnu)[0].block_until_ready()
+    t1 = time.perf_counter()
+    time_custom = (t1 - t0) / 2
+    print("Time (custom VJP):", time_custom)
+    print("Ratio:", time_custom / time_autodiff)
+    print()
