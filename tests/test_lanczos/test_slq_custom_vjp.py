e0b5e03f41bd0db39e13144777ab7299422592e7
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-25 08:35:29 +0100

    Remove matfree.backend imports from SLQ-tests

--- tests/test_lanczos/test_slq_custom_vjp.py
+++ tests/test_lanczos/test_slq_custom_vjp.py
@@ -1,36 +1,37 @@
-"""Tests for the extensions."""
+"""Test the slq-integrand that comes with a custom VJP."""
+
 import jax
+import jax.numpy as jnp
 from matfree import hutchinson, lanczos, test_util
-from matfree.backend import np, prng
 
 from matfree_extensions import lanczos as lanczos_extensions
 
 
 def test_slq_spd_custom_vjp_matches_autodiff(n=10):
-    eigvals = np.arange(0.0, 1.0 + n, step=1.0) + 1.0
+    eigvals = jnp.arange(0.0, 1.0 + n, step=1.0) + 1.0
     A = test_util.symmetric_matrix_from_eigenvalues(eigvals)
 
     def matvec(x, p):
         return p @ x
 
-    x_like = np.ones((len(A),))
+    x_like = jnp.ones((len(A),))
     sampler = hutchinson.sampler_rademacher(x_like, num=1_000)
     order = n // 2
 
     # Reference
-    integrand = lanczos.integrand_spd(np.log, order, matvec)
+    integrand = lanczos.integrand_spd(jnp.log, order, matvec)
     estimate = hutchinson.hutchinson(integrand, sampler)
-    key = prng.prng_key(seed=2)
+    key = jax.random.PRNGKey(seed=2)
     slq_value, slq_gradient = (jax.value_and_grad(estimate, argnums=1))(key, A)
 
     # Implementation
-    integrand = lanczos_extensions.integrand_spd_custom_vjp(np.log, order, matvec)
+    integrand = lanczos_extensions.integrand_spd_custom_vjp(jnp.log, order, matvec)
     estimate = hutchinson.hutchinson(integrand, sampler)
     slq_value_and_grad = (jax.value_and_grad(estimate, argnums=1))(key, A)
 
     # Value should be extremely close
-    assert np.allclose(slq_value, slq_value_and_grad[0], rtol=1e-10, atol=0.0)
+    assert jnp.allclose(slq_value, slq_value_and_grad[0], rtol=1e-10, atol=0.0)
 
     # Gradients tolerances are pretty tight
     rtol, atol = 0.1, 0.1
-    assert np.allclose(slq_gradient, slq_value_and_grad[1], rtol=rtol, atol=atol)
+    assert jnp.allclose(slq_gradient, slq_value_and_grad[1], rtol=rtol, atol=atol)
