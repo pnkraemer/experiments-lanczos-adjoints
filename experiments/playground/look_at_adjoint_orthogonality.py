c2d1312926dab250c524761ed8f67e9c2844a37e
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-31 14:16:10 +0100

    Generate random dxs

--- experiments/playground/look_at_adjoint_orthogonality.py
+++ experiments/playground/look_at_adjoint_orthogonality.py
@@ -8,6 +8,14 @@ from matfree import test_util
 from matfree_extensions import lanczos
 
 
+def random_like(*tree):
+    flat, unflatten = jax.flatten_util.ravel_pytree(tree)
+    flat_like = jax.random.normal(
+        jax.random.PRNGKey(14214213), shape=flat.shape, dtype=flat.dtype
+    )
+    return unflatten(flat_like)
+
+
 def _sym(m):
     return jnp.triu(m) - jnp.diag(0.5 * jnp.diag(m))
 
@@ -24,8 +32,17 @@ vector = jax.random.normal(jax.random.PRNGKey(1), shape=(n,))
 (xs, (alphas, betas)), (x, beta) = lanczos.forward(
     lambda v, p: (p + p.T) @ v, 5, vector, params, reortho=True
 )
+
 print(xs.shape)
 print(alphas.shape)
 print(betas.shape)
 print(x.shape)
 print(beta.shape)
+
+(dxs, (dalphas, dbetas)), (dx, dbeta) = random_like((xs, (alphas, betas)), (x, beta))
+
+print(dxs.shape)
+print(dalphas.shape)
+print(dbetas.shape)
+print(dx.shape)
+print(dbeta.shape)
