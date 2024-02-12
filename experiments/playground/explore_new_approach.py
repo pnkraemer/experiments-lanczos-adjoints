Fix the formula for M -- suddenly, something happens

experiments/playground/explore_new_approach.py
@@ -1,26 +1,9 @@
-"""Investigate whether there is any orthogonality left if dx!=0."""
-
+"""Explore."""
 
 import jax.flatten_util
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
 from matfree import test_util
 from matfree_extensions import lanczos
-from tueplots import figsizes, fontsizes
-
-
-def sym(m):
-    return (m + m.T) / 2
-
-
-def skew(m):
-    return (m - m.T) / 2
-
-
-plt.rcParams.update(figsizes.neurips2023(nrows=2, ncols=2, rel_width=1.0))
-plt.rcParams.update(fontsizes.neurips2023(default_smaller=2))
-
-# jnp.set_printoptions(2)
 
 
 def random_like(*tree):
@@ -35,8 +18,10 @@ def _sym(m):
     return jnp.triu(m) - jnp.diag(0.5 * jnp.diag(m))
 
 
+jnp.set_printoptions(2)
+
 # Set up a test-matrix
-n = 3
+n = 15
 eigvals = jnp.arange(2.0, 2.0 + n)
 # eigvals = eigvals.at[n//2:].set(0.00001)
 matrix = test_util.symmetric_matrix_from_eigenvalues(eigvals)
@@ -45,6 +30,7 @@ params = _sym(matrix)
 
 # Set up an initial vector
 vector = jnp.arange(4.0, 4.0 + n)
+vector /= jnp.linalg.norm(vector)
 
 
 def matvec(v, p):
@@ -53,16 +39,14 @@ def matvec(v, p):
 
 # krylov_depth = 3 * n // 4
 # krylov_depth = n // 2 + 1
-# krylov_depth = n // 2
-krylov_depth = n - 1
+krylov_depth = n // 2
+# krylov_depth = n - 1
 
 # for reortho, axis in zip([False, False], axes):
 for reortho in [True]:
     (xs, (alphas, betas)), (x, beta) = lanczos.forward(
         matvec, krylov_depth, vector, params, reortho=reortho
     )
-    # print(betas)
-    # print(beta)
 
     (dxs, (dalphas, dbetas)), (dx, dbeta) = random_like(
         (xs, (alphas, betas)), (x, beta)
@@ -73,7 +57,7 @@ for reortho in [True]:
     dxs = jnp.concatenate([dxs, dx[None]])
     dbetas = jnp.concatenate([dbetas, dbeta[None]])
 
-    (dv_ref, dp_ref), _ = lanczos._adjoint_pass(
+    (dv_ref, dp_ref), (lambda0, *_) = lanczos._adjoint_pass(
         matvec=matvec,
         params=(params,),
         initvec_norm=jnp.linalg.norm(vector),
@@ -85,8 +69,11 @@ for reortho in [True]:
         dxs=dxs,
         reortho=False,
     )
-    print(dv_ref, dp_ref)
-    (dv, dp), _ = lanczos.matrix_adjoint(
+    # print("Reference:", lambda0)
+    # print()
+    # print()
+
+    (dv, dp), lambdas = lanczos.matrix_adjoint(
         matvec=matvec,
         params=(params,),
         initvec_norm=jnp.linalg.norm(vector),
@@ -96,7 +83,9 @@ for reortho in [True]:
         dalphas=dalphas,
         dbetas=dbetas,
         dxs=dxs,
-        reortho=False,
     )
 
-    print(dv, dp)
+    # print(lambdas[0] - vector * (vector.T @ lambdas[0]))
+    # print(lambdas[0] @ vector)
+    print("product", lambdas @ xs.T)
+    print("product", lambdas.T @ xs)