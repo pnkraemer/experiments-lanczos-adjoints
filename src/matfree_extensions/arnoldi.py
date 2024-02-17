a780fec58f2c5be1ba0ff19cee7215e59a9debee
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-17 14:04:12 +0100

    Draft adjoint of Arnoldi

--- src/matfree_extensions/arnoldi.py
+++ src/matfree_extensions/arnoldi.py
@@ -29,3 +29,69 @@ def forward(A, v, krylov_depth):
         H = H.at[:, i].set(h)
 
     return Q, H, v * length, 1 / initlength
+
+
+def backward(A, vector, krylov_depth, *, Q, H, r, c, dQ, dH, dr, dc):
+    # Transpose the inputs (so code matches maths)
+
+    # Allocate the multipliers
+    Lambda = jnp.zeros_like(Q)
+    Gamma = jnp.zeros_like(dQ.T @ Q)
+
+    e_1, e_K = jnp.eye(krylov_depth)[[0, -1], :]
+
+    alphas = jnp.diag(H)
+    betas = jnp.diag(H, 1)
+
+    # Solve for eta
+    eta = dH @ e_K - Q.T @ dr
+
+    # Solve for L e_K
+    lambda_k = dr + Q @ eta
+
+    # Initialise
+    idx = 1
+
+    # Save result
+    Lambda = Lambda.at[:, -idx].set(lambda_k)
+
+    # Solve or (Gamma + Gamma.T) e_K
+    e1p = -dc * c * jnp.outer(e_1, e_1)
+    tmp = jnp.tril(e1p + H @ dH.T - Lambda.T @ A @ Q - (dQ.T @ Q))
+    tmp = tmp - 0.5 * jnp.diag(jnp.diag(tmp))
+    Gamma = Gamma.at[-idx, :].set(tmp[-idx, :])
+
+    # Solve for the next lambda
+    Xi = dQ.T + (Gamma + Gamma.T) @ Q.T + jnp.outer(eta, r)
+    xi = Xi[-idx]
+
+    lambda_kminus = (xi - (alphas[-idx] * lambda_k - A.T @ lambda_k)) / betas[-idx]
+    lambda_kplus, lambda_k = lambda_k, lambda_kminus
+
+    betas = jnp.concatenate([jnp.ones((1,)), betas])
+    for _ in range(len(alphas) - 1):
+        idx += 1
+        # Save result
+        Lambda = Lambda.at[:, -idx].set(lambda_k)
+
+        # Solve or (Gamma + Gamma.T) e_K
+        tmp = jnp.tril(e1p + H @ dH.T - Lambda.T @ A @ Q - (dQ.T @ Q))
+        tmp = tmp - 0.5 * jnp.diag(jnp.diag(tmp))
+        Gamma = Gamma.at[-idx, :].set(tmp[-idx, :])
+
+        # Solve for the next lambda
+        Xi = dQ.T + (Gamma + Gamma.T) @ Q.T + jnp.outer(eta, r)
+        xi = Xi[-idx]
+        lambda_kminus = (
+            xi
+            - (alphas[-idx] * lambda_k - A.T @ lambda_k)
+            - betas[-(idx - 1)] * lambda_kplus
+        ) / betas[-idx]
+        lambda_kplus, lambda_k = lambda_k, lambda_kminus
+
+    Sigma = (Lambda.T @ Q - dH.T).T
+
+    # Return the solution
+    dv = lambda_k * c
+    dA = Lambda @ Q.T
+    return (dv, dA), (Lambda, lambda_k, Gamma, Sigma, eta)
--- tests/test_arnoldi/test_forward.py
+++ tests/test_arnoldi/test_forward.py
@@ -5,7 +5,7 @@ from matfree_extensions import arnoldi
 jnp.set_printoptions(2, suppress=True)
 
 
-def test_sth(n=10):
+def test_decomposition(n=10):
     A = jax.random.normal(jax.random.PRNGKey(1), shape=(n, n))
     v = jax.random.normal(jax.random.PRNGKey(2), shape=(n,))
     v = v
@@ -23,3 +23,26 @@ def test_sth(n=10):
     assert jnp.allclose(A @ Q - Q @ H - jnp.outer(r, w), 0.0, **tols)
     assert jnp.allclose(Q.T @ Q - jnp.eye(k), 0.0, **tols)
     assert jnp.allclose(Q[:, 0], c * v, **tols)
+
+
+def test_vjp(n=10):
+    A = jax.random.normal(jax.random.PRNGKey(1), shape=(n, n))
+    A = A + A.T
+    v = jax.random.normal(jax.random.PRNGKey(2), shape=(n,))
+    v = v
+
+    k = n // 2
+
+    def fwd(matrix, vector):
+        return arnoldi.forward(matrix, vector, k)
+
+    (Q, H, r, c), vjp = jax.vjp(fwd, A, v)
+
+    da_ref, dv_ref = vjp((Q, H, r, c))
+    print(da_ref)
+    print(dv_ref)
+
+    (dv, da), _ = arnoldi.backward(A, v, k, Q=Q, H=H, r=r, c=c, dQ=Q, dH=H, dr=r, dc=c)
+    print(dv)
+    print(da)
+    assert False
