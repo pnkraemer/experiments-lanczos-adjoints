d6416769d374041c0820e9f526e95a15dc9292fa
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-17 13:51:28 +0100

    Implement Arnoldi

--- src/matfree_extensions/arnoldi.py
+++ src/matfree_extensions/arnoldi.py
@@ -7,6 +7,23 @@ def forward(A, v, krylov_depth):
 
     H = jnp.zeros((k, k))
     Q = jnp.zeros((n, k))
-    r = jnp.zeros((n,))
+    length = 1.0
 
-    return Q, H, r
+    for i in range(k):
+        # Save
+        Q = Q.at[:, i].set(v)
+
+        # Evaluate
+        v = A @ v
+
+        # Orthonormalise
+        h = Q.T @ v
+        v = v - Q @ h
+        length = jnp.linalg.norm(v)
+        v /= length
+
+        # Save
+        h = h.at[i + 1].set(length)
+        H = H.at[:, i].set(h)
+
+    return Q, H, v * length
--- tests/test_arnoldi/test_forward.py
+++ tests/test_arnoldi/test_forward.py
@@ -2,10 +2,13 @@ import jax
 import jax.numpy as jnp
 from matfree_extensions import arnoldi
 
+jnp.set_printoptions(2, suppress=True)
+
 
 def test_sth(n=10):
     A = jax.random.normal(jax.random.PRNGKey(1), shape=(n, n))
     v = jax.random.normal(jax.random.PRNGKey(2), shape=(n,))
+    v = v / jnp.linalg.norm(v)
 
     k = n // 2
     Q, H, r = arnoldi.forward(A, v, k)
@@ -13,7 +16,9 @@ def test_sth(n=10):
     assert Q.shape == (n, k)
     assert H.shape == (k, k)
 
+    small_value = jnp.sqrt(jnp.finfo(jnp.dtype(H)).eps)
+    tols = {"atol": small_value, "rtol": small_value}
+
     w = jnp.eye(k)[-1]
-    assert jnp.allclose(A @ Q - Q @ H - jnp.outer(r, w), 0.0)
-    assert jnp.allclose(Q.T @ Q - jnp.eye(k), 0.0)
-    assert False
+    assert jnp.allclose(A @ Q - Q @ H - jnp.outer(r, w), 0.0, **tols)
+    assert jnp.allclose(Q.T @ Q - jnp.eye(k), 0.0, **tols)
