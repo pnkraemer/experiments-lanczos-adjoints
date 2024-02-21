2fa6f67b3546f019b9de0dc88f1315b3bcb61077
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-21 14:35:34 +0100

    Leave todos in the PDE experiment

--- experiments/playground/plot_pde_solution_via_matrix_exponential.py
+++ experiments/playground/plot_pde_solution_via_matrix_exponential.py
@@ -1,13 +1,26 @@
+# todo: use a proper optimizer
+# todo: matrix-free implementation
+# todo: use more parameters
+# todo: 2d
+# todo: have a comparison
+
+import functools
+
 import jax
 import jax.numpy as jnp
 import jax.scipy.linalg
 
+# Set discretisation parameters
 dx = 0.01
 dt = 0.1
 ts = jnp.arange(0.0, 1.0 + dt, step=dt)
 xs = jnp.arange(0.0, 1.0 + dx, step=dx)
-# coeff = -0.1
 
+# Set problem parameters
+y0 = jnp.exp(-100 * (xs - 0.5) ** 2)
+coeff_true = 0.1
+
+# Discretise
 stencil = jnp.asarray([-1, 2.0, -1]) / dx**2
 
 
@@ -16,32 +29,39 @@ def matvec(x):
     return jnp.convolve(stencil, x_padded, mode="valid")
 
 
-y0 = jnp.exp(-100 * (xs - 0.5) ** 2)
-
-
-def sol(t, coeff):
-    A = jax.jacfwd(lambda s: matvec(s))(y0)
-    return jax.scipy.linalg.expm(t * A * coeff) @ y0
-
-
-coeff_true = -0.1
-data = sol(1.0, coeff_true) + 0.01 * jax.random.normal(
-    jax.random.PRNGKey(1), shape=y0.shape
-)
+# Parameter-to-solution/error operators
 
 
 @jax.jit
 @jax.value_and_grad
-def parameter_to_error(c):
-    solution = sol(1.0, c)
-    diff = solution - data
+def parameter_to_error(inputs, targets):
+    solution = parameter_to_solution(1.0, inputs)
+    diff = solution - targets
     return jnp.dot(diff, diff)
 
 
-c0 = -1.0
-val, grad = parameter_to_error(c0)
+@jax.jit
+def parameter_to_solution(t, c):
+    A = jax.jacfwd(lambda s: matvec(s))(y0)
+    return jax.scipy.linalg.expm(-c * t * A) @ y0
+
+
+# Create an optimization problem
+
+solution_true = parameter_to_solution(1.0, coeff_true)
+noise = 0.01 * jax.random.normal(jax.random.PRNGKey(1), shape=y0.shape)
+data = solution_true + noise
+loss_value_and_grad = functools.partial(parameter_to_error, targets=data)
+
+# Optimize
+
+learning_rate = 0.01
+coeff = 0.2  # initial guess
+val, grad = loss_value_and_grad(coeff)  # JIT-compile
 
 while jnp.linalg.norm(grad) > jnp.sqrt(jnp.finfo(val.dtype).eps):
-    val, grad = parameter_to_error(c0)
-    c0 -= 0.1 * grad
-    print(c0)
+    value, gradient = loss_value_and_grad(coeff)
+
+    # Gradient descent
+    coeff = coeff - learning_rate * gradient
+    print(coeff)
