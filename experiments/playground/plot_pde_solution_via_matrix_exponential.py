0535ba6af79eeba40bef4bd104f9c1518c07b0fa
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-21 17:04:09 +0100

    Have a rough draft of a PDE plot

--- experiments/playground/plot_pde_solution_via_matrix_exponential.py
+++ experiments/playground/plot_pde_solution_via_matrix_exponential.py
@@ -1,6 +1,10 @@
 # todo: use a different differential equation?
-# todo: 2d
-# todo: have a comparison
+# todo: remove constant parameters from param vector
+# todo: implement different matrix exponentials
+# todo: use a "proper" loss function
+# todo: implement a comparison algorithm
+# todo: make this example just minimally shinier and it could end up in the paper!
+
 
 import functools
 
@@ -24,16 +28,15 @@ xs_flat, unflatten_xs = jax.flatten_util.ravel_pytree(xs)
 coeff_true = {"init": jnp.asarray([0.5, 0.5]), "rhs": 0.01}
 _coeff, unflatten_p = jax.flatten_util.ravel_pytree(coeff_true)
 
-krylov_depth = 50
+krylov_depth = 100
+print(xs_flat.shape)
 
 
 # Set problem parameters
 def init(x, s):
     x = unflatten_xs(x)
     diff = x - s[:, None]
-    fx = jax.vmap(lambda d: jnp.exp(-100 * jnp.dot(d, d)), in_axes=-1, out_axes=-1)(
-        diff
-    )
+    fx = jax.vmap(lambda d: jnp.exp(-50 * jnp.dot(d, d)), in_axes=-1, out_axes=-1)(diff)
     return jax.flatten_util.ravel_pytree(fx)[0]
 
 
@@ -65,17 +68,7 @@ algorithm = arnoldi.arnoldi(rhs, krylov_depth, reortho="full", custom_vjp=True)
 def parameter_to_error(params, targets):
     solution = parameter_to_solution(1.0, params)
     diff = solution - targets
-    return jnp.dot(diff, diff)
-
-
-#
-# @jax.jit
-# def parameter_to_solution(t, params):
-#     coeff_ = unflatten_p(params)
-#     y0 = init(xs, coeff_["init"])
-#     A = jax.jacfwd(lambda s: rhs(s, coeff_["rhs"]))(y0)
-#     return jax.scipy.linalg.expm(t * A) @ y0
-#
+    return jnp.sqrt(jnp.dot(diff, diff) / diff.size)
 
 
 @jax.jit
@@ -84,13 +77,24 @@ def parameter_to_solution(t, params):
     y0 = init(xs, coeff_["init"])
     Q, H, _r, c = algorithm(y0, coeff_["rhs"])
     e1 = jnp.eye(len(H))[0, :]
-    return c * Q @ jax.scipy.linalg.expm(t * H) @ e1
+
+    H = (H + H.T) / 2
+    eigvals, eigvecs = jnp.linalg.eigh(H)
+
+    expm = eigvecs @ jnp.diag(jnp.exp(t * eigvals)) @ eigvecs.T
+
+    # print(expm)
+    # print(eigvecs @ jnp.diag(eigvals) @ eigvecs.T)
+    # print(H)
+    # assert False
+    # expm = jax.scipy.linalg.expm(t * H)
+    return c * Q @ expm @ e1
 
 
 # Create an optimization problem
 coeff_true_flat, _unflatten = jax.flatten_util.ravel_pytree(coeff_true)
 solution_true = parameter_to_solution(1.0, coeff_true_flat)
-noise = 1e-3 * jax.random.normal(jax.random.PRNGKey(2), shape=solution_true.shape)
+noise = 1e-8 * jax.random.normal(jax.random.PRNGKey(2), shape=solution_true.shape)
 data = solution_true + noise
 loss_value_and_grad = functools.partial(parameter_to_error, targets=data)
 
@@ -114,28 +118,31 @@ fig, (axes, axes_before, axes_after) = plt.subplots(
 fig.suptitle(f"N={xs.size //2} points; K={krylov_depth} Krylov-depth")
 
 
+def magnitude(x):
+    return jnp.log10(jnp.abs(x) + jnp.finfo(x.dtype).eps)
+
+
 y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff_true_flat))(ts)
-for y1_, ax in zip(y1, axes):
-    ax.contourf(mesh[0], mesh[1], unflatten_y(y1_))
+plot_kwargs = {"vmin": jnp.amin(magnitude(y1)), "vmax": jnp.amax(magnitude(y1))}
+print(plot_kwargs)
+for t_, y1_, ax in zip(ts, y1, axes):
+    ax.set_title(f"$t={t_:2F}$")
+    ax.contourf(mesh[0], mesh[1], magnitude(unflatten_y(y1_)), **plot_kwargs)
 
 axes[0].set_ylabel("Truth")
 
 
 # initial guess
-noise = 1e-1 * jax.random.normal(jax.random.PRNGKey(2), shape=coeff_true_flat.shape)
-coeff = coeff_true_flat + noise
-coeff = jnp.asarray([0.7, 0.7, 1e-3])
-
+coeff = jnp.asarray([0.8, 0.8, 1e-3])
 
 y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff))(ts)
-for t_, y1_, ax in zip(ts, y1, axes_before):
-    ax.set_title(f"$t={t_}$")
-    ax.contourf(mesh[0], mesh[1], unflatten_y(y1_))
+for y1_, ax in zip(y1, axes_before):
+    ax.contourf(mesh[0], mesh[1], magnitude(unflatten_y(y1_)), **plot_kwargs)
 
-axes_before[0].set_ylabel("Truth")
+axes_before[0].set_ylabel("Initial guess")
 
 # Optimizer
-learning_rate = 1e-2
+learning_rate = 1e-1
 optimizer = optax.adam(learning_rate)
 opt_state = optimizer.init(coeff)
 
@@ -148,19 +155,20 @@ value, gradient = loss_value_and_grad(coeff)  # JIT-compile
 
 gradient = jnp.ones_like(gradient)
 count = 0
-for count in range(10):
+for count in range(20):
     value, gradient = loss_value_and_grad(coeff)
 
     updates, opt_state = optimizer.update(gradient, opt_state)
     coeff = optax.apply_updates(coeff, updates)
 
-    print(count, coeff, jnp.linalg.norm(gradient))
+    print(count, coeff, magnitude(jnp.linalg.norm(gradient)))
 
 
 y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff))(ts)
 
 for y1_, ax in zip(y1, axes_after):
-    ax.contourf(mesh[0], mesh[1], unflatten_y(y1_))
+    ax.contourf(mesh[0], mesh[1], magnitude(unflatten_y(y1_)), **plot_kwargs)
 axes_after[0].set_ylabel("Recovered")
+
 
 plt.show()
