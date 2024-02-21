e8cd303fbe5b7a5b1be146fb4f2146851916649c
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-21 16:33:41 +0100

    Something is training...

--- experiments/playground/plot_pde_solution_via_matrix_exponential.py
+++ experiments/playground/plot_pde_solution_via_matrix_exponential.py
@@ -13,7 +13,7 @@ import optax
 from matfree_extensions import arnoldi
 
 # Set discretisation parameters
-dx = 1e-1
+dx = 1e-2
 xs_1 = jnp.arange(0.0, 1.0 + dx, step=dx)
 xs_2 = jnp.arange(0.0, 1.0 + dx, step=dx)
 mesh = jnp.stack(jnp.meshgrid(xs_1, xs_2))
@@ -21,10 +21,10 @@ xs = mesh.reshape((2, -1))
 xs_flat, unflatten_xs = jax.flatten_util.ravel_pytree(xs)
 
 
-coeff_true = {"init": jnp.asarray([0.5, 0.5]), "rhs": 0.1}
+coeff_true = {"init": jnp.asarray([0.5, 0.5]), "rhs": 0.01}
 _coeff, unflatten_p = jax.flatten_util.ravel_pytree(coeff_true)
 
-krylov_depth = 10
+krylov_depth = 50
 
 
 # Set problem parameters
@@ -44,8 +44,9 @@ stencil = jnp.asarray([[0.0, -1.0, 0.0], [-1, 2.0, -1], [0.0, -1.0, 0.0]]) / dx*
 _, unflatten_y = jax.flatten_util.ravel_pytree(jnp.meshgrid(xs_1, xs_2)[0])
 
 
+@jax.jit
 def rhs(x, d):
-    d = 0.01
+    d = 1e-3
     x = unflatten_y(x)
     x_padded = jnp.pad(x, 1, mode="constant", constant_values=0.0)
 
@@ -89,7 +90,7 @@ def parameter_to_solution(t, params):
 # Create an optimization problem
 coeff_true_flat, _unflatten = jax.flatten_util.ravel_pytree(coeff_true)
 solution_true = parameter_to_solution(1.0, coeff_true_flat)
-noise = 1e-5 * jax.random.normal(jax.random.PRNGKey(2), shape=solution_true.shape)
+noise = 1e-3 * jax.random.normal(jax.random.PRNGKey(2), shape=solution_true.shape)
 data = solution_true + noise
 loss_value_and_grad = functools.partial(parameter_to_error, targets=data)
 
@@ -100,13 +101,20 @@ print("....", loss_value_and_grad(coeff_true_flat))
 y0 = init(xs_flat, coeff_true["init"])
 
 
-ts = jnp.arange(0.0, 1.0, step=0.1)
-y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff_true_flat))(ts)
+ts = jnp.arange(0.0, 1.2, step=0.2)
 
-fig, (axes, axes_after) = plt.subplots(
-    nrows=2, ncols=len(ts), figsize=(len(ts) * 2, 3), sharex=True, sharey=True
+fig, (axes, axes_before, axes_after) = plt.subplots(
+    nrows=3,
+    ncols=len(ts),
+    figsize=(len(ts) * 2, 5),
+    sharex=True,
+    sharey=True,
+    constrained_layout=True,
 )
+fig.suptitle(f"N={xs.size //2} points; K={krylov_depth} Krylov-depth")
 
+
+y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff_true_flat))(ts)
 for y1_, ax in zip(y1, axes):
     ax.contourf(mesh[0], mesh[1], unflatten_y(y1_))
 
@@ -114,11 +122,20 @@ axes[0].set_ylabel("Truth")
 
 
 # initial guess
-noise = 1e-2 * jax.random.normal(jax.random.PRNGKey(2), shape=coeff_true_flat.shape)
+noise = 1e-1 * jax.random.normal(jax.random.PRNGKey(2), shape=coeff_true_flat.shape)
 coeff = coeff_true_flat + noise
+coeff = jnp.asarray([0.7, 0.7, 1e-3])
+
+
+y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff))(ts)
+for t_, y1_, ax in zip(ts, y1, axes_before):
+    ax.set_title(f"$t={t_}$")
+    ax.contourf(mesh[0], mesh[1], unflatten_y(y1_))
+
+axes_before[0].set_ylabel("Truth")
 
 # Optimizer
-learning_rate = 1e-3
+learning_rate = 1e-2
 optimizer = optax.adam(learning_rate)
 opt_state = optimizer.init(coeff)
 
@@ -131,15 +148,13 @@ value, gradient = loss_value_and_grad(coeff)  # JIT-compile
 
 gradient = jnp.ones_like(gradient)
 count = 0
-while jnp.linalg.norm(gradient) > jnp.power(jnp.finfo(value.dtype).eps, 0.5):
+for count in range(10):
     value, gradient = loss_value_and_grad(coeff)
 
     updates, opt_state = optimizer.update(gradient, opt_state)
     coeff = optax.apply_updates(coeff, updates)
 
-    if count % 1 == 0:
-        print(count, coeff, jnp.linalg.norm(gradient))
-    count += 1
+    print(count, coeff, jnp.linalg.norm(gradient))
 
 
 y1 = jax.vmap(lambda t_: parameter_to_solution(t_, coeff))(ts)
