b383fef43e44fb53ab8bdaa8c9b017365d5feab9
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-21 10:25:09 +0100

    Refine the projection-plot

--- experiments/arnoldi_adjoints/display_the_effect_of_projection/display.py
+++ experiments/arnoldi_adjoints/display_the_effect_of_projection/display.py
@@ -1,4 +1,5 @@
 import functools
+import os
 
 import jax.flatten_util
 import jax.numpy as jnp
@@ -27,7 +28,7 @@ def matvec(x, p):
     return p @ x
 
 
-nrows = 15
+nrows = 21
 
 fig = plt.figure(layout="constrained", dpi=200)
 fig_single, fig_double = fig.subfigures(nrows=1, ncols=2)
@@ -41,25 +42,33 @@ for x64, subfig, title, cmap in zip(
 ):
     jax.config.update("jax_enable_x64", x64)
 
-    axes = subfig.subplot_mosaic([["none", "full"]])
+    axes = subfig.subplot_mosaic([[*(["none"] * 10), "arrow", *(["full"] * 10)]])
     axes["none"].set_title("No proj.")
     axes["full"].set_title("Full proj.")
 
+    arrowprops = {"arrowstyle": "->"}
+    axes["arrow"].annotate("", xy=(0.0, 1.0), xytext=(0, 0), arrowprops=arrowprops)
+    axes["arrow"].set_xticks(())
+    axes["arrow"].set_yticks(())
+    axes["arrow"].spines["top"].set_visible(False)
+    axes["arrow"].spines["bottom"].set_visible(False)
+    axes["arrow"].spines["left"].set_visible(False)
+    axes["arrow"].spines["right"].set_visible(False)
+
     for key in ["none", "full"]:
-        axes[key].set_xlabel("Row index", fontsize="small")
+        axes[key].set_xlabel("Column index", fontsize="small")
         axes[key].set_xticks((0, (nrows - 1) // 2, nrows - 1))
         axes[key].set_xticklabels((1, nrows // 2, nrows))
 
-        axes[key].set_ylabel("Column index", fontsize="small")
+        axes[key].set_ylabel("Row index", fontsize="small")
         axes[key].set_yticks((0, (nrows - 1) // 2, nrows - 1))
         axes[key].set_yticklabels((1, nrows // 2, nrows))
 
     for reortho in ["none", "full"]:
-        fwd = functools.partial(arnoldi.forward, reortho=reortho)
+        fwd = functools.partial(arnoldi.forward, reortho="full")
         adj = functools.partial(arnoldi.adjoint, reortho=reortho)
 
         # Set up a test problem
-        # matrix = exp_util.hilbert(nrows)
         eigvals = 2.0 ** jnp.arange(-nrows // 2, nrows // 2, 1)
         matrix = test_util.symmetric_matrix_from_eigenvalues(eigvals)
         vector = jnp.ones((nrows,)) / jnp.sqrt(nrows)
@@ -88,8 +97,8 @@ for x64, subfig, title, cmap in zip(
 
         # Context for the values:
         cond = jnp.linalg.cond(matrix)
-        context = jnp.log10(cond) + vmin
-        context = f"{round(context, 2): 0.2f}"
+        cond_vs_eps_val = jnp.log10(cond) + vmin
+        cond_vs_eps = f"{round(cond_vs_eps_val, 2): 0.2f}"
 
         plot_kwargs = {
             "vmin": vmin,
@@ -100,8 +109,15 @@ for x64, subfig, title, cmap in zip(
         colors = axes[reortho].imshow(magnitude(adj_ortho), **plot_kwargs)
         axes[reortho].set_aspect("equal")
 
-    subfig.suptitle(f"{title} (cond. vs eps.: {context})", fontsize="medium")
-    cbar = subfig.colorbar(colors, ticks=(vmin, 0, -vmin), extend="both")
+    subfig.suptitle(f"{title} (cond. vs eps.: {cond_vs_eps})", fontsize="medium")
+    cbar = subfig.colorbar(colors, ticks=(vmin, 0, -vmin))
     cbar.minorticks_on()
     cbar.ax.set_title("Log-error", fontsize="x-small")
+
+
+directory = exp_util.matching_directory(__file__, "figures/")
+os.makedirs(directory, exist_ok=True)
+
+plt.savefig(f"{directory}/figure.pdf")
+
 plt.show()
