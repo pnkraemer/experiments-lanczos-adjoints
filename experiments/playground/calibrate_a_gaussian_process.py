91f6c9f014e8dfa41be55a19e6810323676dd383
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-22 11:37:57 +0100

    Plot the fit

--- experiments/playground/calibrate_a_gaussian_process.py
+++ experiments/playground/calibrate_a_gaussian_process.py
@@ -5,33 +5,48 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
 
-def f(x):
-    return jnp.sin(10 * x**2)
+def f(x, *, p=2):
+    return jnp.sin(p * x**2)
 
 
-def k(x, y, scale_in, scale_out):
+def k(x, y, *, scale_in, scale_out):
     diff = x - y
     log_k = scale_in * jnp.dot(diff, diff)
     return scale_out * jnp.exp(log_k)
 
 
-xs = jnp.linspace(0, 1, num=100, endpoint=True)
-ys = f(xs)
+def parametrize(fun, **parameters):
+    return functools.partial(fun, **parameters)
 
 
-def condition(inputs, targets, scale_in, scale_out, observation_noise):
-    k_p = functools.partial(k, scale_in=scale_in, scale_out=scale_out)
-    k_p_1 = jax.vmap(k_p, in_axes=(0, None), out_axes=0)
-    k_p_2 = jax.vmap(k_p_1, in_axes=(None, 1), out_axes=1)
+def vect(fun):
+    tmp = jax.vmap(fun, in_axes=(0, None), out_axes=0)
+    return jax.vmap(tmp, in_axes=(None, 1), out_axes=1)
 
-    K = k_p_2(inputs[..., None], inputs[None, ...])
+
+def condition(inputs, targets, *, kernel, observation_noise):
+    K = kernel(inputs[..., None], inputs[None, ...])
     shift = observation_noise * jnp.eye(len(K))
     return jnp.linalg.solve(K + shift, targets)
 
 
-ms = condition(xs, ys, 1, 1, 1.0)
+# Training data
+xs = jnp.linspace(0, 1, num=100, endpoint=True)
+ys = f(xs)
+
+# Parametrize and condition
+params = {"scale_in": 1.0, "scale_out": 1.0}
+k_p = parametrize(k, **params)
+k_vect = vect(k_p)
+coeff = condition(xs, ys, kernel=k_vect, observation_noise=1.0)
+
+# Evaluate
+xs_new = jnp.linspace(0, 1, num=33, endpoint=True)
+ys_new = k_vect(xs_new[:, None], xs[None, :]) @ coeff
 
 
-plt.plot(xs, ys)
-plt.plot(xs, ms)
+plt.plot(xs, ys, label="Truth")
+plt.plot(xs_new, ys_new, label="Estimate")
+plt.xlim((jnp.amin(xs), jnp.amax(xs)))
+plt.legend()
 plt.show()
