efccbea47a36b544ff198869df96f7cc0d3c546e
Nicholas Krämer <nicokraemer@gmx.net>
2024-02-23 13:53:39 +0100

    Find a working configuration for GP parameter estimation

--- experiments/applications/calibrate_a_gaussian_process/calibrate_a_gaussian_process.py
+++ experiments/applications/calibrate_a_gaussian_process/calibrate_a_gaussian_process.py
@@ -13,98 +13,66 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from matfree_extensions import gp
 
-#
-#
-#
-#
-# def sample(key_, p, *, kfun, inputs):
-#     k_partial = parametrize(kfun, p["scale_in"], p["scale_out"])
-#     k_vectorised = vect(k_partial)
-#
-#     K = k_vectorised(inputs[..., None], inputs[None, ...])
-#     K += observation_noise * jnp.eye(len(K))
-#
-#     xi = jax.random.normal(key_, shape=inputs.shape)
-#     return jnp.linalg.solve(K, xi)
-#     pass
-
-
-def create_loss(inputs, targets, kfun, unflatten_p):
-    def loss_fun(p_flat):
-        params_ = unflatten_p(p_flat)
-        return parameter_to_solution(**params_)
-
-    def parameter_to_solution(observation_noise, **p):
-        k_partial = kfun(**p)
-        k_vectorised = vect(k_partial)
-        return log_likelihood(kernel=k_vectorised, observation_noise=observation_noise)
-
-    def log_likelihood(kernel, observation_noise):
-        K = kernel(inputs[..., None], inputs[None, ...])
-        shift = observation_noise * jnp.eye(len(K))
-
-        coeffs = jnp.linalg.solve(K + shift, targets)
-        mahalanobis = jnp.dot(targets, coeffs)
-
-        _sign, entropy = jnp.linalg.slogdet(K)
-        return -(mahalanobis + entropy), (coeffs, jnp.linalg.cond(K))
-
-    return loss_fun
-
-
-def vect(fun):
-    tmp = jax.vmap(fun, in_axes=(0, None), out_axes=0)
-    return jax.vmap(tmp, in_axes=(None, 1), out_axes=1)
-
-
 # Parameters
-params_true = {"observation_noise": 1e-3, "scale_in": 1e1, "scale_out": 1e-1}
-params_true_flat, unflatten_p = jax.flatten_util.ravel_pytree(params_true)
-kernel = gp.kernel_quadratic_rational()
+params_obs = {"noise": 1e-7}
+params_kernel = {"scale_in": 1e1, "scale_out": 1e0}
+params_true = {"observation": params_obs, "kernel": params_kernel}
+kernel = gp.kernel_quadratic_rational(gram_matrix=True)
 
 
 # Training data
-key = jax.random.PRNGKey(1)
-xs = jnp.linspace(0, 1, num=100, endpoint=True)
-
-ys = xs**2
-# ys = sample(key, params_true, kfun=k, inputs=xs)
+key = jax.random.PRNGKey(2)
+xs = jnp.linspace(0, 1, num=100, endpoint=True)[..., None]
+ys = gp.process_sample(key, **params_obs, inputs=xs, kernel=kernel(**params_kernel))
 
 
 # Parametrize and condition
-params = {"observation_noise": 1.0, "scale_in": 1e0, "scale_out": 1e0}
-params_flat, _ = jax.flatten_util.ravel_pytree(params)
-loss = create_loss(xs, ys, kernel, unflatten_p)
-loss_value_and_grad = jax.jit(jax.value_and_grad(loss, has_aux=True))
+params_init1 = {"noise": 1.0}
+params_init2 = {"scale_in": 1e1, "scale_out": 1e-1}
+params = {"observation": params_init1, "kernel": params_init2}
+params_flat, unflatten_p = jax.flatten_util.ravel_pytree(params)
+
+
+@jax.jit
+@jax.value_and_grad
+def loss_value_and_grad(p_flat):
+    params_ = unflatten_p(p_flat)
+    kernel_p = kernel(**params_["kernel"])
+    obs_p = params_["observation"]
+    score, _coeff = gp.log_likelihood(xs, ys, kernel=kernel_p, **obs_p)
+    return -score
+
 
 # Train
-num_steps = 1_000
+num_steps = 110
 for i in range(num_steps):
-    (score, (_coeff, cond)), grad = loss_value_and_grad(params_flat)
-    params_flat += 0.01 * grad
+    score, grad = loss_value_and_grad(params_flat)
+    params_flat += 0.001 * grad
 
     if i % (num_steps // 10) == 0:
         print("index =", i)
         print("\tscore =", score)
-        print("\tcond =", jnp.log10(cond))
         print("\tparams =", unflatten_p(params_flat))
         print()
 
+
+# Condition
 # Read the solution
-_score, (coeff, _cond) = loss(params_flat)
 parameters = unflatten_p(params_flat)
-scale_in, scale_out = parameters["scale_in"], parameters["scale_out"]
+params_obs = parameters["observation"]
+params_kernel = parameters["kernel"]
+kernel_calibrated = kernel(**params_kernel)
+cond = gp.process_condition(xs, ys, kernel=kernel_calibrated, noise=params_obs["noise"])
+mean_cond, cov_cond = cond
 
+# Evaluate
 
-# Evaluate the solution
-k_p = kernel(scale_in=scale_in, scale_out=scale_out)
-k_vect = vect(k_p)
-xs_new = jnp.linspace(0, 1, num=33, endpoint=True)
-ys_new = k_vect(xs_new[:, None], xs[None, :]) @ coeff
+xs_new = jnp.linspace(0, 1, num=200, endpoint=True)[..., None]
+ys_new = mean_cond(xs_new)
+
 
 # Plot
-plt.title(f"Score={jnp.round(score, 2):2f}")
-plt.plot(xs, ys, label="Truth")
+plt.plot(xs, ys, "x", label="Truth")
 plt.plot(xs_new, ys_new, label="Estimate")
 plt.xlim((jnp.amin(xs), jnp.amax(xs)))
 plt.legend()
--- src/matfree_extensions/gp.py
+++ src/matfree_extensions/gp.py
@@ -1,25 +1,89 @@
+import jax
 import jax.numpy as jnp
 
 
-def kernel_quadratic_exponential():
+def kernel_quadratic_exponential(*, gram_matrix: bool):
     def parametrize(*, scale_in, scale_out):
         def k(x, y):
             diff = x - y
-            log_k = scale_in * jnp.dot(diff, diff)
-            return scale_out * jnp.exp(log_k)
+            log_k = scale_in**2 * jnp.dot(diff, diff)
+            return scale_out**2 * jnp.exp(log_k)
 
+        if gram_matrix:
+            return _vmap_gram(k)
         return k
 
     return parametrize
 
 
-def kernel_quadratic_rational():
+def kernel_quadratic_rational(*, gram_matrix: bool):
     def parametrize(*, scale_in, scale_out):
         def k(x, y):
             diff = x - y
-            tmp = scale_in**2 * jnp.dot(diff, diff)
-            return scale_out**2 / (1 + tmp)
+            tmp = scale_in * jnp.dot(diff, diff)
+            return scale_out / (1 + tmp)
 
+        if gram_matrix:
+            return _vmap_gram(k)
         return k
 
     return parametrize
+
+
+def _vmap_gram(fun):
+    tmp = jax.vmap(fun, in_axes=(0, None), out_axes=0)
+    return jax.vmap(tmp, in_axes=(None, 1), out_axes=1)
+
+
+def process_sample(key, *, noise, inputs, kernel, shape=()):
+    assert inputs.ndim == 2
+    key_sample, key_noise = jax.random.split(key, num=2)
+
+    K = kernel(inputs, inputs.T)
+    U, s, Vt = jnp.linalg.svd(K)
+
+    def transform(a, b):
+        return U @ jnp.diag(jnp.sqrt(s)) @ Vt @ a + noise * b
+
+    if shape == ():
+        xi = jax.random.normal(key_sample, shape=(len(K),))
+        eta = jax.random.normal(key_noise, shape=(len(K),))
+        return transform(xi, eta)
+
+    xi = jax.random.normal(key_sample, shape=(*shape, len(K)))
+    eta = jax.random.normal(key_noise, shape=(*shape, len(K)))
+    return jax.vmap(transform)(xi, eta)
+
+
+def process_condition(inputs, targets, *, noise, kernel):
+    assert inputs.ndim == 2
+
+    K = kernel(inputs, inputs.T)
+    K += noise * jnp.eye(len(K))
+    coeff = jnp.linalg.solve(K, targets)
+
+    def mean(x):
+        assert x.ndim == 2
+        return kernel(x, inputs.T) @ coeff
+
+    def cov(x, y):
+        assert x.ndim == 2
+        assert y.ndim == 2
+        k_xx = kernel(x, y.T)
+        k_xy = jnp.linalg.solve(K, kernel(inputs, y.T))
+        return k_xx - kernel(x, inputs.T) @ k_xy
+
+    return mean, cov
+
+
+def log_likelihood(inputs, targets, *, kernel, noise):
+    assert inputs.ndim == 2
+    assert targets.ndim == 1
+    K = kernel(inputs, inputs.T)
+    shift = noise * jnp.eye(len(K))
+
+    coeffs = jnp.linalg.solve(K + shift, targets)
+    mahalanobis = jnp.dot(targets, coeffs)
+    _sign, entropy = jnp.linalg.slogdet(K)
+
+    return -(mahalanobis + entropy), (coeffs, jnp.linalg.cond(K))
