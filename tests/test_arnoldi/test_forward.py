fe5ce25b7b5014fa01071be7726f20cdc93f2fcb
Nicholas Krämer <nicokraemer@gmx.net>
2024-03-27 08:56:37 +0100

    Simplify the data-type logic in arnoldi-forward tests

--- tests/test_arnoldi/test_forward.py
+++ tests/test_arnoldi/test_forward.py
@@ -9,44 +9,11 @@ from matfree_extensions import arnoldi, exp_util
 @pytest_cases.parametrize("nrows", [10])
 @pytest_cases.parametrize("krylov_depth", [1, 5, 10])
 @pytest_cases.parametrize("reortho", ["none", "full"])
-def test_decomposition_is_satisfied(nrows, krylov_depth, reortho):
+@pytest_cases.parametrize("dtype", [float, complex])
+def test_decomposition_is_satisfied(nrows, krylov_depth, reortho, dtype):
     # Create a well-conditioned test-matrix
-    A = jax.random.normal(jax.random.PRNGKey(1), shape=(nrows, nrows))
-    v = jax.random.normal(jax.random.PRNGKey(2), shape=(nrows,))
-
-    # Decompose
-    Q, H, r, c = arnoldi.forward(
-        lambda s, p: p @ s, krylov_depth, v, A, reortho=reortho
-    )
-
-    # Assert shapes
-    assert Q.shape == (nrows, krylov_depth)
-    assert H.shape == (krylov_depth, krylov_depth)
-    assert r.shape == (nrows,)
-    assert c.shape == ()
-
-    # Tie the test-strictness to the floating point accuracy
-    small_value = jnp.sqrt(jnp.finfo(jnp.dtype(H)).eps)
-    tols = {"atol": small_value, "rtol": small_value}
-
-    # Test the decompositions
-    e0, ek = jnp.eye(krylov_depth)[[0, -1], :]
-    assert jnp.allclose(A @ Q - Q @ H - jnp.outer(r, ek), 0.0, **tols)
-    assert jnp.allclose(Q.T @ Q - jnp.eye(krylov_depth), 0.0, **tols)
-    assert jnp.allclose(Q @ e0, c * v, **tols)
-
-
-@pytest_cases.parametrize("nrows", [10])
-@pytest_cases.parametrize("krylov_depth", [1, 5, 10])
-@pytest_cases.parametrize("reortho", ["none", "full"])
-def test_decomposition_is_satisfied_for_complex_types(nrows, krylov_depth, reortho):
-    # Create a well-conditioned test-matrix
-    A1 = jax.random.normal(jax.random.PRNGKey(1), shape=(nrows, nrows))
-    A2 = jax.random.normal(jax.random.PRNGKey(2), shape=(nrows, nrows))
-    A = A1 + A2 * 1j
-    v1 = jax.random.normal(jax.random.PRNGKey(3), shape=(nrows,))
-    v2 = jax.random.normal(jax.random.PRNGKey(4), shape=(nrows,))
-    v = v1 + v2 * 1j
+    A = jax.random.normal(jax.random.PRNGKey(1), shape=(nrows, nrows), dtype=dtype)
+    v = jax.random.normal(jax.random.PRNGKey(2), shape=(nrows,), dtype=dtype)
 
     # Decompose
     Q, H, r, c = arnoldi.forward(
