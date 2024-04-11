73edc3675b420f299494985421a9c686d6ab6600
h-roy <h-roy@github.com>
2024-04-11 16:58:23 +0200

    sine with la

--- experiments/applications/linearised_laplace/simple_sin.py
+++ experiments/applications/linearised_laplace/simple_sin.py
@@ -39,12 +39,17 @@ if __name__=="__main__":
 
     # Create model
     model = MLP(out_dims=1)
-    variables = model.init(jax.random.PRNGKey(42), x_train)
-    y = model.apply(variables, x_train)
+    variables_dict = model.init(jax.random.PRNGKey(42), x_train)
+    variables, unflatten = jax.flatten_util.ravel_pytree(variables_dict)
+
+    def apply_fn(params: jax.Array, x: jax.Array):
+        return model.apply(unflatten(params), x)
+    
+    y = apply_fn(variables, x_train)
 
     # Train model via optax
     def loss_fn(params, x_, y_):
-        y_pred = model.apply(params, x_).reshape((-1,))
+        y_pred = apply_fn(params, x_).reshape((-1,))
         return jnp.mean((y_pred - y_)**2, axis=0)
     
 
@@ -53,23 +58,76 @@ if __name__=="__main__":
     n_epochs = 200
     key = jax.random.PRNGKey(412576)
     batch_size = num_data
+    value_and_grad = jax.jit(jax.value_and_grad(loss_fn))
     for epoch in range(n_epochs):
         # Subsample data
         key, subkey = jax.random.split(key)
         idx = jax.random.choice(subkey, x_train.shape[0], (batch_size,), replace=False)
 
-        loss, grad = jax.value_and_grad(loss_fn)(variables, x_train[idx], y_train[idx])
+        loss, grad = value_and_grad(variables, x_train[idx], y_train[idx])
         updates, optimizer_state = optimizer.update(grad, optimizer_state)
         variables = optax.apply_updates(variables, updates)
         if epoch % 10 == 0:
             print(f"Epoch {epoch}, loss {loss:.3f}")
-            y = model.apply(variables, x_train)
 
     # Plot predictions
     x_val = jnp.linspace(0, 2*jnp.pi, 1000)
-    y_pred = model.apply(variables, x_val)
+    y_pred = apply_fn(variables, x_val)
     plt.plot(x_val, f(x_val))
     plt.plot(x_val, y_pred)
     plt.savefig(f"{directory}/predixcyions.pdf")
     
+    # Construct the GGN Matrix
+    def calib_loss(log_alpha):
+        D = len(variables)
+        log_prior = D / 2 * log_alpha - 0.5 * _alpha(log_alpha) * jnp.dot(variables, variables)
+        M = ggn_fn(log_alpha)
+        _sign, logdet = jnp.linalg.slogdet(M)
+        log_marginal = log_prior - 0.5 * logdet
+        return -log_marginal 
+    
+    def data_likelihood(log_alpha, x_test, y_test):
+        # GP in weight space
+        mean = variables
+        cov = jnp.linalg.inv(ggn_fn(log_alpha))
 
+        # GP in y space
+        mean_fn = (apply_fn)(mean, x_test).reshape((-1,))
+        J_test = (jax.jacfwd(lambda p: apply_fn(p, x_test))(variables)).squeeze()
+        cov_fn = J_test @ cov @ J_test.T
+
+        # Compute the likelihood
+        import scipy.stats 
+        import numpy as np 
+        return scipy.stats.multivariate_normal.logpdf(
+            np.asarray(y_test),
+            np.asarray(mean_fn),
+            np.asarray(cov_fn + cov_fn.T) / 2 + np.eye(100)*1e-6,
+            allow_singular=True,
+        )
+        return jax.scipy.stats.multivariate_normal.logpdf(y_test, mean_fn, cov_fn, allow_singular=True)
+    
+    def ggn_fn(log_alpha):
+        J = (jax.jacfwd(lambda p: apply_fn(p, x_train))(variables)).squeeze()
+        GGN = J.T @ J
+        return GGN + _alpha(log_alpha) * jnp.eye(GGN.shape[0])
+    
+    def _alpha(log_alpha):
+        return 1e-3 + jnp.exp(log_alpha)
+    
+
+    # Optimize the calibration loss
+    log_alpha = 0.0
+    optimizer = optax.adam(1e-1)
+    optimizer_state = optimizer.init(log_alpha)
+    value_and_grad = jax.jit(jax.value_and_grad(calib_loss))
+    n_epochs = 200
+    for epoch in range(n_epochs):
+        loss, grad = value_and_grad(log_alpha)
+        updates, optimizer_state = optimizer.update(grad, optimizer_state)
+        log_alpha = optax.apply_updates(log_alpha, updates)
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}, loss {loss:.3f}, alpha {_alpha(log_alpha):.3f}")
+
+    print("Log Likelihood (before):", data_likelihood(0.0, x_train, y_train))
+    print("Log Likelihood (after):", data_likelihood(log_alpha, x_train, y_train))
