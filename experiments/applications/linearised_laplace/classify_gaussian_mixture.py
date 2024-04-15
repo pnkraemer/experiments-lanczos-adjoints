25ae8f1103aac8feb760285fb130a78b0c7a236a
Nicholas Krämer <nicokraemer@gmx.net>
2024-04-15 15:16:23 +0200

    Move much of the training logic to a bnn_util.py

--- experiments/applications/linearised_laplace/classify_gaussian_mixture.py
+++ experiments/applications/linearised_laplace/classify_gaussian_mixture.py
@@ -13,77 +13,79 @@ import jax.numpy as jnp
 import optax
 from matfree_extensions import bnn_util, exp_util
 
-if __name__ == "__main__":
-    # Make directories
-    directory = exp_util.matching_directory(__file__, "figures/")
-    os.makedirs(directory, exist_ok=True)
+# Make directories
+directory = exp_util.matching_directory(__file__, "figures/")
+os.makedirs(directory, exist_ok=True)
 
-    # Create data
-    num_data = 100
-    key_1, key_2 = jax.random.split(jax.random.PRNGKey(1))
-    m = 2.0
-    mu_1 = jnp.array((-m, -m))
-    mu_2 = jnp.array((m, m))
-    x_1 = jax.random.normal(key_1, (num_data, 2)) + mu_1[None, :]
-    y_1 = jnp.asarray(num_data * [[1, 0]])
-    x_2 = jax.random.normal(key_2, (num_data, 2)) + mu_2[None, :]
-    y_2 = jnp.asarray(num_data * [[0, 1]])
-    x_train = jnp.concatenate([x_1, x_2], axis=0)
-    y_train = jnp.concatenate([y_1, y_2], axis=0)
+# Create data
+num_data = 100
+key_1, key_2 = jax.random.split(jax.random.PRNGKey(1))
+m = 2.0
+mu_1 = jnp.array((-m, -m))
+mu_2 = jnp.array((m, m))
+x_1 = jax.random.normal(key_1, (num_data, 2)) + mu_1[None, :]
+y_1 = jnp.asarray(num_data * [[1, 0]])
+x_2 = jax.random.normal(key_2, (num_data, 2)) + mu_2[None, :]
+y_2 = jnp.asarray(num_data * [[0, 1]])
+x_train = jnp.concatenate([x_1, x_2], axis=0)
+y_train = jnp.concatenate([y_1, y_2], axis=0)
 
-    # Create model
-    model = bnn_util.MLP(out_dims=2)
-    variables_dict = model.init(jax.random.PRNGKey(42), x_train)
-    variables, unflatten = jax.flatten_util.ravel_pytree(variables_dict)
+# Create model
+model_init, model_apply = bnn_util.model_mlp(output_dimensions=2)
+variables_dict = model_init(jax.random.PRNGKey(42), x_train)
+variables, unflatten = jax.flatten_util.ravel_pytree(variables_dict)
 
-    # Loss and accuracy
+# Optimise
 
-    # Optimise
+optimizer = optax.adam(1e-2)
+optimizer_state = optimizer.init(variables)
+n_epochs = 200
+key = jax.random.PRNGKey(412576)
+batch_size = num_data
 
-    optimizer = optax.adam(1e-2)
-    optimizer_state = optimizer.init(variables)
-    n_epochs = 200
-    key = jax.random.PRNGKey(412576)
-    batch_size = num_data
 
-    def loss_p(v, x, y):
-        return jnp.mean(
-            jax.vmap(bnn_util.loss_single)(model.apply(unflatten(v), x), y), axis=0
-        )
+def loss_p(v, x, y):
+    y_pred = model_apply(unflatten(v), x)
+    return bnn_util.loss_training_cross_entropy(y_pred, y)
 
-    loss_value_and_grad = jax.jit(jax.value_and_grad(loss_p, argnums=0))
 
-    for epoch in range(n_epochs):
-        # Subsample data
-        key, subkey = jax.random.split(key)
-        idx = jax.random.choice(subkey, x_train.shape[0], (batch_size,), replace=False)
+loss_value_and_grad = jax.jit(jax.value_and_grad(loss_p, argnums=0))
 
-        # Optimiser step
-        loss, grad = loss_value_and_grad(variables, x_train[idx], y_train[idx])
-        updates, optimizer_state = optimizer.update(grad, optimizer_state)
-        variables = optax.apply_updates(variables, updates)
+for epoch in range(n_epochs):
+    # Subsample data
+    key, subkey = jax.random.split(key)
+    idx = jax.random.choice(subkey, x_train.shape[0], (batch_size,), replace=False)
 
-        # Look at intermediate results
-        if epoch % 10 == 0:
-            acc = bnn_util.accuracy(
-                model.apply(unflatten(variables), x_train[idx]), y_train[idx]
-            )
-            print(f"Epoch {epoch}, loss {loss:.3f}, accuracy {acc:.3f}")
+    # Optimiser step
+    loss, grad = loss_value_and_grad(variables, x_train[idx], y_train[idx])
+    updates, optimizer_state = optimizer.update(grad, optimizer_state)
+    variables = optax.apply_updates(variables, updates)
 
-    # Optimize the calibration loss
-    log_alpha = 0.0
-    optimizer = optax.adam(1e-1)
-    optimizer_state = optimizer.init(log_alpha)
+    # Look at intermediate results
+    if epoch % 10 == 0:
+        y_pred = model_apply(unflatten(variables), x_train[idx])
+        acc = bnn_util.metric_accuracy(y_pred, y_train[idx])
+        print(f"Epoch {epoch}, loss {loss:.3f}, accuracy {acc:.3f}")
 
-    calib_loss = bnn_util.calib_loss(model.apply, unflatten)
-    value_and_grad = jax.jit(jax.value_and_grad(calib_loss, argnums=0))
+# Optimize the calibration loss
+log_alpha = 0.0
+optimizer = optax.adam(1e-1)
+optimizer_state = optimizer.init(log_alpha)
 
-    n_epochs = 200
-    for epoch in range(n_epochs):
-        loss, grad = value_and_grad(log_alpha, variables, x_train, y_train)
-        updates, optimizer_state = optimizer.update(grad, optimizer_state)
-        log_alpha = optax.apply_updates(log_alpha, updates)
-        if epoch % 10 == 0:
-            print(f"Epoch {epoch}, loss {loss:.3f}, alpha {log_alpha:.3f}")
+calib_loss = bnn_util.loss_calibration(
+    model_fun=model_apply,
+    param_unflatten=unflatten,
+    loss_single=bnn_util.loss_training_cross_entropy_single,
+    hyperparam_unconstrain=lambda s: 1e-3 + jnp.exp(s),
+)
+value_and_grad = jax.jit(jax.value_and_grad(calib_loss, argnums=0))
 
-    print("Successfull!!!!! :) :) :) ")
+n_epochs = 200
+for epoch in range(n_epochs):
+    loss, grad = value_and_grad(log_alpha, variables, x_train, y_train)
+    updates, optimizer_state = optimizer.update(grad, optimizer_state)
+    log_alpha = optax.apply_updates(log_alpha, updates)
+    if epoch % 10 == 0:
+        print(f"Epoch {epoch}, loss {loss:.3f}, alpha {log_alpha:.3f}")
+
+print("Successfull!!!!! :) :) :) ")
--- src/matfree_extensions/bnn_util.py
+++ src/matfree_extensions/bnn_util.py
@@ -5,7 +5,7 @@ import jax
 import jax.numpy as jnp
 
 
-class MLP(flax.linen.Module):
+class _MLP(flax.linen.Module):
     out_dims: int
 
     @flax.linen.compact
@@ -16,23 +16,32 @@ class MLP(flax.linen.Module):
         return flax.linen.Dense(self.out_dims)(x)
 
 
-def f(x):
-    return jnp.sin(x)
+def model_mlp(output_dimensions):
+    model = _MLP(output_dimensions)
+    return model.init, model.apply
 
 
-def loss_single(y_pred, y_data):
+def metric_accuracy(y_pred, y_data):
+    return jnp.mean(jnp.argmax(y_pred, axis=-1) == jnp.argmax(y_data, axis=-1))
+
+
+def loss_training_cross_entropy(y_pred, y_data):
+    loss_single = jax.vmap(loss_training_cross_entropy_single)(y_pred, y_data)
+    return jnp.mean(loss_single, axis=0)
+
+
+def loss_training_cross_entropy_single(y_pred, y_data):
     logprobs = jax.nn.log_softmax(y_pred, axis=-1)
     return -jnp.sum(logprobs * y_data, axis=-1)
 
 
-def accuracy(y_pred, y_data):
-    return jnp.mean(jnp.argmax(y_pred, axis=-1) == jnp.argmax(y_data, axis=-1))
-
-
-def calib_loss(model_fun, unflatten):
+def loss_calibration(
+    *, model_fun, param_unflatten, loss_single, hyperparam_unconstrain
+):
     def loss(a, variables, x_train, y_train):
+        alpha = hyperparam_unconstrain(a)
         tmp1 = len(variables) / 2 * a
-        tmp2 = -0.5 * _alpha(a) * jnp.dot(variables, variables)
+        tmp2 = -0.5 * alpha * jnp.dot(variables, variables)
         log_prior = tmp1 + tmp2
 
         M = ggn_fn(a, variables, x_train, y_train)
@@ -42,13 +51,13 @@ def calib_loss(model_fun, unflatten):
         return -log_marginal
 
     def ggn_fn(a, variables, x_train, y_train):
-        model_pred = model_fun(unflatten(variables), x_train)
-        H = jax.vmap(jax.hessian(loss_single, argnums=0))(model_pred, y_train)
-        J = jax.jacfwd(lambda v: model_fun(unflatten(v), x_train))(variables)
-        ggn_summands = jax.vmap(lambda j, h: j.T @ h @ j)(J, H)
-        return jnp.sum(ggn_summands, axis=0) + _alpha(a) * jnp.eye(J.shape[-1])
+        alpha = hyperparam_unconstrain(a)
+        model_pred = model_fun(param_unflatten(variables), x_train)
 
-    def _alpha(a):
-        return 1e-3 + jnp.exp(a)
+        H = jax.vmap(jax.hessian(loss_single, argnums=0))(model_pred, y_train)
+        J = jax.jacfwd(lambda v: model_fun(param_unflatten(v), x_train))(variables)
+
+        ggn_summands = jax.vmap(lambda j, h: j.T @ h @ j)(J, H)
+        return jnp.sum(ggn_summands, axis=0) + alpha * jnp.eye(J.shape[-1])
 
     return loss
