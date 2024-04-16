3defc1bf5065838a0f9c237bb2d11f664d23e226
Nicholas Krämer <nicokraemer@gmx.net>
2024-04-16 13:42:49 +0200

    Parse configuration via argparse

--- experiments/playground/benchmark_keops_versus_batched_gram.py
+++ experiments/playground/benchmark_keops_versus_batched_gram.py
@@ -1,34 +1,38 @@
 """Compare the efficiency of GPyTorch+KeOps to gp.gram_matvec_map()."""
 
+# todo: plot the results
+# todo: collect memory consumption
+# todo: inside a jit, and on GPU/CPU,
+#  is calling a map() instead of vmap() really necessary?
+# todo: understand how to benchmark memory in torch
+# todo: matvec_gram_batches poorly:
+#  Instead of (vmap(k)(x,y)) @ v, it should call vmap(k(-,y) @ v)(x)
+# todo: submit as a script
+#  (I think the memory stats might be contaminated by other processes)
+# todo: assert values are identical
+
+import argparse
 import time
 
 import gpytorch.kernels
-import gpytorch.kernels.keops  # requires pip install pykeops
+import gpytorch.kernels.keops
 import jax
 import jax.numpy as jnp
 import torch
 from matfree_extensions import gp
 
-num_matvecs = 3
 
-
-def print_ts(t):
+def print_ts(t: jax.Array, *, label: str, num_matvecs: int):
     t = jnp.asarray(t)
-    print(
-        f"\t{jnp.mean(t):.3f} +/- {jnp.std(t):.3f} "
-        f"(mean +/- std of {num_matvecs} runs; all in seconds)"
-    )
+    m, s = jnp.mean(t), jnp.std(t)
+    print(f"{label}:\t{m:.3f} +/- {s:.3f} sec (mean +/- std of {num_matvecs} runs)")
 
 
-for num in 2 ** jnp.arange(10, 16):
-    print(f"\nN = {num}")
-    print("-----------")
-
+def run(N, *, num_matvecs):
     shape_in = ()
-    x = torch.randn((num, *shape_in))
-    v = torch.randn((num,))
+    x = torch.randn((N, *shape_in))
+    v = torch.randn((N,))
 
-    print("GPyTorch + Keops (Matern32)")
     kernel = gpytorch.kernels.keops.MaternKernel(nu=1.5)
     K = kernel(x)
 
@@ -38,16 +42,14 @@ for num in 2 ** jnp.arange(10, 16):
         _ = K @ v
         t1 = time.perf_counter()
         ts.append(t1 - t0)
-    print_ts(ts)
-
-    print("Matfree (Matern32)")
+    ts_torch = jnp.asarray(ts)
 
     kernel, params = gp.kernel_scaled_matern_32(shape_in=(), shape_out=())
-    matvec = gp.gram_matvec_map_over_batch(batch_size=16)
+    matvec = gp.gram_matvec_map()
     K = jax.jit(matvec(kernel(**params)))
     x_ = jnp.asarray(x.detach().numpy())
     v_ = jnp.asarray(v.detach().numpy())
-    _ = K(x_, x_, v_)
+    _ = K(x_, x_, v_)  # pre-compile
 
     ts = []
     for _ in range(num_matvecs):
@@ -56,5 +58,23 @@ for num in 2 ** jnp.arange(10, 16):
         vv.block_until_ready()
         t1 = time.perf_counter()
         ts.append(t1 - t0)
-    print_ts(ts)
-    print("\n")
+    ts_matfree = jnp.asarray(ts)
+    return ts_torch, ts_matfree
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_matvecs", type=int, required=True)
+    parser.add_argument("--matrix_size_min", type=int, required=True)
+    parser.add_argument("--matrix_size_max", type=int, required=True)
+    args = parser.parse_args()
+
+    powers = jnp.arange(args.matrix_size_min, args.matrix_size_max)
+    matrix_sizes = 2**powers
+    for idx, num in zip(powers, matrix_sizes):
+        t_torch, t_matfree = run(num, num_matvecs=args.num_matvecs)
+        print(f"\nI = {idx}, N = {num}")
+        print("------------------")
+        print_ts(t_torch, label="Keops", num_matvecs=args.num_matvecs)
+        print_ts(t_matfree, label="JAX", num_matvecs=args.num_matvecs)
+        print()
