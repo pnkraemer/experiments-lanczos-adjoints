965239c8d8f1de0dbbac04bb41982651cf888666
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-04 12:12:38 +0200

    Plot

--- experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec_per_data_dim.py
+++ experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec_per_data_dim.py
@@ -9,8 +9,8 @@
 # todo: submit as a script
 #  (I think the memory stats might be contaminated by other processes)
 
-import os
 import argparse
+import os
 import time
 from typing import Callable
 
@@ -19,7 +19,7 @@ import gpytorch.kernels.keops
 import jax
 import jax.numpy as jnp
 import torch
-from matfree_extensions.util import gp_util, exp_util
+from matfree_extensions.util import exp_util, gp_util
 
 
 def print_ts(t: jax.Array, *, label: str, num_runs: int):
@@ -81,44 +81,51 @@ if __name__ == "__main__":
 
         print(f"\ndim = {dim}")
         print("------------------")
-        if not "dim" in results:
+        if "dim" not in results:
             results["dim"] = []
         results["dim"].append(dim)
 
-
         label = "Matfree (via JAX's map)"
         matvec = gp_util.gram_matvec_map()
-        t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+        t = time_matfree(
+            seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs
+        )
         print_ts(t, label=label, num_runs=args.num_runs)
-        if not label in results:
+        if label not in results:
             results[label] = []
         results[label].append(t)
-            
+
         for bnum in reversed([1, 10, 200, 4000]):
             if args.data_size >= bnum:
                 label = f"Matfree (via map-over-vmap; {bnum} batches)"
                 matvec = gp_util.gram_matvec_map_over_batch(num_batches=bnum)
-                t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+                t = time_matfree(
+                    seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs
+                )
                 print_ts(t, label=label, num_runs=args.num_runs)
-                if not label in results:
+                if label not in results:
                     results[label] = []
                 results[label].append(t)
-                
+
         label = "Matfree (via JAX's vmap)"
         matvec = gp_util.gram_matvec_full_batch()
-        t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+        t = time_matfree(
+            seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs
+        )
         print_ts(t, label=label, num_runs=args.num_runs)
-        if not label in results:
+        if label not in results:
             results[label] = []
         results[label].append(t)
-            
+
         gpytorch_label = "GPyTorch (via pykeops)"
-        gpytorch_t = time_gpytorch_via_pykeops(seed, args.data_size, (dim,), num_runs=args.num_runs)
+        gpytorch_t = time_gpytorch_via_pykeops(
+            seed, args.data_size, (dim,), num_runs=args.num_runs
+        )
         print_ts(gpytorch_t, label=gpytorch_label, num_runs=args.num_runs)
-        if not label in results:
+        if label not in results:
             results[label] = []
         results[label].append(t)
-        
+
         print()
 
 print("Saving to a file")
--- experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/plot_matvec.py
+++ experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/plot_matvec.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matfree_extensions.util import exp_util
 
 labels = [
-    "num",
+    "dim",
     "Matfree (via JAX's map)",
     "Matfree (via JAX's vmap)",
     "GPyTorch (via pykeops)",
@@ -13,7 +13,46 @@ labels = labels  # + [f"Matfree (via map-over-vmap; {bnum} batches)" for bnum in
 print(labels)
 
 directory = exp_util.matching_directory(__file__, "results/")
-LABEL = f"num_runs_{5}_data_dim_{1}"
+
+
+LABEL = f"num_runs_{3}_data_size_{40_000}"
+
+results = {}
+for x in labels:
+    path = f"{directory}matvec_per_data_dim_{LABEL}_{x}.npy"
+    results[x] = jnp.load(path)
+
+print(results)
+inputs = results["dim"]
+for key, value in results.items():
+    if key != "dim":
+        print(inputs.shape)
+        print(value.shape)
+        if " map" in key:
+            plt.semilogy(inputs[: len(value)], jnp.amin(value, axis=1), label=key)
+        else:
+            plt.semilogy(inputs, jnp.amin(value, axis=1), label=key)
+        # plt.loglog(inputs, jnp.amax(value, axis=1), label=key)
+
+plt.grid(which="major", linestyle="dotted")
+plt.legend()
+plt.show()
+print(results)
+
+################################################################################
+
+
+labels = [
+    "num",
+    "Matfree (via JAX's map)",
+    "Matfree (via JAX's vmap)",
+    "GPyTorch (via pykeops)",
+]
+labels = labels  # + [f"Matfree (via map-over-vmap; {bnum} batches)" for bnum in [1, 16, 256]]
+
+print(labels)
+
+LABEL = f"num_runs_{3}_data_dim_{1}"
 
 results = {}
 for x in labels:
@@ -26,10 +65,13 @@ for key, value in results.items():
     if key != "num":
         print(inputs.shape)
         print(value.shape)
+        if " map" in key:
+            plt.loglog(inputs[: len(value)], jnp.amin(value, axis=1), label=key)
+        else:
+            plt.loglog(inputs, jnp.amin(value, axis=1), label=key)
+        # plt.loglog(inputs, jnp.amax(value, axis=1), label=key)
 
-        plt.loglog(inputs, jnp.amin(value, axis=1), label=key)
-
-
+plt.grid(which="major", linestyle="dotted")
 plt.legend()
 plt.show()
 print(results)
