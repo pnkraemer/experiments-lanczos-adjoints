c6d47268d1ec661f81ec860667b8ccd3d0370e4a
Peter Nicholas Kramer <pekra@dtu.dk>
2024-05-04 11:35:35 +0200

    Benchmark the matvec per data-dimension

--- experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec_per_data_dim.py
+++ experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec_per_data_dim.py
@@ -68,59 +68,53 @@ def time_matfree(prng_seed, N: int, shape_in: tuple, mv, *, num_runs: int):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_runs", type=int, required=True)
-    parser.add_argument("--matrix_size_min", type=int, required=True)
-    parser.add_argument("--matrix_size_max", type=int, required=True)
-    parser.add_argument("--data_dim", type=int, required=True)
+    parser.add_argument("--data_dim_min", type=int, required=True)
+    parser.add_argument("--data_dim_max", type=int, required=True)
+    parser.add_argument("--data_size", type=int, default=40_000)
     args = parser.parse_args()
-
-    powers = jnp.arange(args.matrix_size_min, args.matrix_size_max)
-    num_runs = args.num_runs
-
-    shape_in = (args.data_dim,)
 
     results = {}
 
-    matrix_sizes = 2**powers
-    for idx, num in zip(powers, matrix_sizes):
+    for dim in range(args.data_dim_min, args.data_dim_max):
         # Use the current "size" as a seed
-        seed = idx
+        seed = dim
 
-        print(f"\nI = {idx}, N = {num}")
+        print(f"\ndim = {dim}")
         print("------------------")
-        if not "num" in results:
-            results["num"] = []
-        results["num"].append(num)
+        if not "dim" in results:
+            results["dim"] = []
+        results["dim"].append(dim)
 
-        if num <= 200_000:
-            label = "Matfree (via JAX's map)"
-            matvec = gp_util.gram_matvec_map()
-            t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
-            print_ts(t, label=label, num_runs=num_runs)
-            if not label in results:
-                results[label] = []
-            results[label].append(t)
+
+        label = "Matfree (via JAX's map)"
+        matvec = gp_util.gram_matvec_map()
+        t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+        print_ts(t, label=label, num_runs=args.num_runs)
+        if not label in results:
+            results[label] = []
+        results[label].append(t)
             
-        for bnum in reversed([1, 16, 256, 4096]):
-            if num >= bnum:
+        for bnum in reversed([1, 10, 200, 4000]):
+            if args.data_size >= bnum:
                 label = f"Matfree (via map-over-vmap; {bnum} batches)"
                 matvec = gp_util.gram_matvec_map_over_batch(num_batches=bnum)
-                t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
-                print_ts(t, label=label, num_runs=num_runs)
+                t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+                print_ts(t, label=label, num_runs=args.num_runs)
                 if not label in results:
                     results[label] = []
                 results[label].append(t)
                 
         label = "Matfree (via JAX's vmap)"
         matvec = gp_util.gram_matvec_full_batch()
-        t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
-        print_ts(t, label=label, num_runs=num_runs)
+        t = time_matfree(seed, args.data_size, (dim,), mv=matvec, num_runs=args.num_runs)
+        print_ts(t, label=label, num_runs=args.num_runs)
         if not label in results:
             results[label] = []
         results[label].append(t)
             
         gpytorch_label = "GPyTorch (via pykeops)"
-        gpytorch_t = time_gpytorch_via_pykeops(seed, num, shape_in, num_runs=num_runs)
-        print_ts(gpytorch_t, label=gpytorch_label, num_runs=num_runs)
+        gpytorch_t = time_gpytorch_via_pykeops(seed, args.data_size, (dim,), num_runs=args.num_runs)
+        print_ts(gpytorch_t, label=gpytorch_label, num_runs=args.num_runs)
         if not label in results:
             results[label] = []
         results[label].append(t)
@@ -130,8 +124,8 @@ if __name__ == "__main__":
 print("Saving to a file")
 directory = exp_util.matching_directory(__file__, "results/")
 os.makedirs(directory, exist_ok=True)
-LABEL = f"num_runs_{args.num_runs}_data_dim_{args.data_dim}"
+LABEL = f"num_runs_{args.num_runs}_data_size_{args.data_size}"
 
 for key, value in results.items():
-    path = f"{directory}/matvec_{LABEL}_{key}.npy"
+    path = f"{directory}/matvec_per_data_dim_{LABEL}_{key}.npy"
     jnp.save(path, jnp.asarray(value))
---
+++ experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec_per_data_size.py
@@ -0,0 +1,137 @@
+"""Compare the efficiency of GPyTorch+KeOps to gp.gram_matvec_map()."""
+
+# todo: plot the results
+
+# todo: collect memory consumption
+
+# todo: understand how to benchmark memory in torch
+
+# todo: submit as a script
+#  (I think the memory stats might be contaminated by other processes)
+
+import os
+import argparse
+import time
+from typing import Callable
+
+import gpytorch.kernels
+import gpytorch.kernels.keops
+import jax
+import jax.numpy as jnp
+import torch
+from matfree_extensions.util import gp_util, exp_util
+
+
+def print_ts(t: jax.Array, *, label: str, num_runs: int):
+    t = jnp.asarray(t)
+    amin, median, amax = jnp.amin(t), jnp.median(t), jnp.amax(t)
+    msg = f"{amin:.1e} < {median:.1e} < {amax:.1e}"
+    description = f"| minimum < median < maximum of {num_runs} runs | {label}"
+    print(msg, description)
+
+
+def time_matvec(mv: Callable, vec: jax.Array, params, *, num_runs: int):
+    ts = []
+    _ = mv(vec, params)  # for potential pre-compilation
+    for _ in range(num_runs):
+        t0 = time.perf_counter()
+        _ = mv(vec, params)
+        t1 = time.perf_counter()
+
+        ts.append(t1 - t0)
+    return jnp.asarray(ts)
+
+
+def time_gpytorch_via_pykeops(prng_seed, N: int, shape_in: tuple, *, num_runs: int):
+    torch.manual_seed(prng_seed)
+    x = torch.randn((N, *shape_in)).cuda()
+    vec = torch.randn((N,)).cuda()
+    kernel = gpytorch.kernels.keops.MaternKernel(nu=1.5)
+    kernel = gpytorch.kernels.ScaleKernel(kernel).cuda()
+    return time_matvec(lambda v, p: kernel(p) @ v, vec, x, num_runs=num_runs)
+
+
+def time_matfree(prng_seed, N: int, shape_in: tuple, mv, *, num_runs: int):
+    key = jax.random.PRNGKey(prng_seed)
+    key1, key2 = jax.random.split(key)
+    x = jax.random.normal(key1, shape=(N, *shape_in))
+    vec = jax.random.normal(key1, shape=(N,))
+    kernel, params = gp_util.kernel_scaled_matern_32(shape_in=shape_in, shape_out=())
+    fun = jax.jit(mv(kernel(**params)))
+
+    def matvec_fun(v, p):
+        return fun(p, p, v).block_until_ready()
+
+    return time_matvec(matvec_fun, vec, x, num_runs=num_runs)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_runs", type=int, required=True)
+    parser.add_argument("--matrix_size_min", type=int, required=True)
+    parser.add_argument("--matrix_size_max", type=int, required=True)
+    parser.add_argument("--data_dim", type=int, required=True)
+    args = parser.parse_args()
+
+    powers = jnp.arange(args.matrix_size_min, args.matrix_size_max)
+    num_runs = args.num_runs
+
+    shape_in = (args.data_dim,)
+
+    results = {}
+
+    matrix_sizes = 2**powers
+    for idx, num in zip(powers, matrix_sizes):
+        # Use the current "size" as a seed
+        seed = idx
+
+        print(f"\nI = {idx}, N = {num}")
+        print("------------------")
+        if not "num" in results:
+            results["num"] = []
+        results["num"].append(num)
+
+        if num <= 200_000:
+            label = "Matfree (via JAX's map)"
+            matvec = gp_util.gram_matvec_map()
+            t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
+            print_ts(t, label=label, num_runs=num_runs)
+            if not label in results:
+                results[label] = []
+            results[label].append(t)
+            
+        for bnum in reversed([1, 16, 256, 4096]):
+            if num >= bnum:
+                label = f"Matfree (via map-over-vmap; {bnum} batches)"
+                matvec = gp_util.gram_matvec_map_over_batch(num_batches=bnum)
+                t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
+                print_ts(t, label=label, num_runs=num_runs)
+                if not label in results:
+                    results[label] = []
+                results[label].append(t)
+                
+        label = "Matfree (via JAX's vmap)"
+        matvec = gp_util.gram_matvec_full_batch()
+        t = time_matfree(seed, num, shape_in, mv=matvec, num_runs=num_runs)
+        print_ts(t, label=label, num_runs=num_runs)
+        if not label in results:
+            results[label] = []
+        results[label].append(t)
+            
+        gpytorch_label = "GPyTorch (via pykeops)"
+        gpytorch_t = time_gpytorch_via_pykeops(seed, num, shape_in, num_runs=num_runs)
+        print_ts(gpytorch_t, label=gpytorch_label, num_runs=num_runs)
+        if not label in results:
+            results[label] = []
+        results[label].append(t)
+        
+        print()
+
+print("Saving to a file")
+directory = exp_util.matching_directory(__file__, "results/")
+os.makedirs(directory, exist_ok=True)
+LABEL = f"num_runs_{args.num_runs}_data_dim_{args.data_dim}"
+
+for key, value in results.items():
+    path = f"{directory}/matvec_per_data_size_{LABEL}_{key}.npy"
+    jnp.save(path, jnp.asarray(value))
