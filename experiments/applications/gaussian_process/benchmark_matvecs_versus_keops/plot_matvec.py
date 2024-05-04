b333256ab86481bd1e8c845118f32ec73572396f
Peter Nicholas Kramer <pekra@dtu.dk>
2024-05-04 13:53:23 +0200

    Rerun the matvec benchmarks

--- experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/plot_matvec.py
+++ experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/plot_matvec.py
@@ -10,7 +10,7 @@ labels = {
     "matfree_map": "Ours (via map)",
     "matfree_vmap": "Ours (via vmap)",
 }
-plt.rcParams.update(figsizes.neurips2024(ncols=2))
+plt.rcParams.update(figsizes.neurips2024(ncols=3, nrows=2))
 plt.rcParams.update(fontsizes.neurips2024())
 plt.rcParams.update(axes.lines())
 plt.rcParams.update(axes.grid())
@@ -18,32 +18,35 @@ plt.rcParams.update(axes.legend())
 
 directory = exp_util.matching_directory(__file__, "results/")
 
-fig, axes = plt.subplot_mosaic([["per_size", "per_dim"]], sharex=False, sharey=True)
+fig, axes = plt.subplot_mosaic([["per_size1","per_size4", "per_dim"]], sharex=False, sharey=True)
 
 
 print("Plotting data varied by dataset size")
 num_runs = 3  # match to the existing data
-data_dim = 1  # match to the existing data
-data_sizes = 2 ** jnp.arange(1, 6, dtype=int)  # match to the existing data
-for method in ["gpytorch", "matfree_map", "matfree_vmap"]:
-    times = []
-    for size in data_sizes:
-        title = f"matvec_num_runs_{num_runs}_data_size_{size}_data_dim_{data_dim}"
-        x = jnp.load(f"{directory}{title}_{method}.npy")
-        times.append(x)
-    times = jnp.asarray(times)
-    axes["per_size"].semilogy(data_sizes, jnp.amin(times, axis=1), label=labels[method])
+for data_dim in [1, 4]:
+    data_sizes = 2 ** jnp.arange(10, 18, dtype=int)  # match to the existing data
+    for method in ["gpytorch", "matfree_map", "matfree_vmap"]:
+        times = []
+        for size in data_sizes:
+            title = f"matvec_num_runs_{num_runs}_data_size_{size}_data_dim_{data_dim}"
+            x = jnp.load(f"{directory}{title}_{method}.npy")
+            times.append(x)
+        times = jnp.asarray(times)
+        axes[f"per_size{data_dim}"].loglog(data_sizes, jnp.amin(times, axis=1), label=labels[method])
 
-axes["per_size"].legend(fontsize="xx-small")
-axes["per_size"].set_title(f"Dataset dimension: {data_dim}")
-axes["per_size"].set_xlabel("Dataset size")
-axes["per_size"].set_ylabel("Run time (sec)")
-axes["per_size"].grid(which="major")
+    axes[f"per_size{data_dim}"].set_title(f"Dataset dimension: {data_dim}")
+
+    if data_dim == 1:
+        axes[f"per_size{data_dim}"].set_ylabel("Run time (sec)")
+
+    axes[f"per_size{data_dim}"].legend(fontsize="xx-small")
+    axes[f"per_size{data_dim}"].set_xlabel("Dataset size")
+    axes[f"per_size{data_dim}"].grid(which="major")
 
 print("Plotting data varied by dataset dim")
 num_runs = 3  # match to the existing data
-data_size = 64  # match to the existing data
-data_dims = jnp.arange(1, 8, dtype=int)  # match to the existing data
+data_size = 65536  # match to the existing data
+data_dims = jnp.arange(1, 15, dtype=int)  # match to the existing data
 
 for method in ["gpytorch", "matfree_map", "matfree_vmap"]:
     times = []
@@ -52,7 +55,7 @@ for method in ["gpytorch", "matfree_map", "matfree_vmap"]:
         x = jnp.load(f"{directory}{title}_{method}.npy")
         times.append(x)
     times = jnp.asarray(times)
-    axes["per_dim"].semilogy(data_dims, jnp.amin(times, axis=1), label=labels[method])
+    axes["per_dim"].loglog(data_dims, jnp.amin(times, axis=1), label=labels[method])
 
 axes["per_dim"].legend(fontsize="xx-small")
 axes["per_dim"].set_title(f"Dataset size: {data_size}")
@@ -63,5 +66,3 @@ print("Saving to a file")
 directory = exp_util.matching_directory(__file__, "figures/")
 os.makedirs(directory, exist_ok=True)
 plt.savefig(f"{directory}/figure_matvec.pdf")
-
-plt.show()
--- scripts/benchmark_matvec_vs_keops.sh
+++ scripts/benchmark_matvec_vs_keops.sh
@@ -13,10 +13,12 @@ done
 
 # Large-scale runs
 
-for i in {1..16}; do \
+for i in {1..15}; do \
     python experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec.py --num_runs 3 --log_data_size 16 --data_dim $i ;
 done
 
-for i in {10..21}; do \
+for i in {10..19}; do \
+    python experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec.py --num_runs 3 --log_data_size $i --data_dim 1 ;
     python experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec.py --num_runs 3 --log_data_size $i --data_dim 2 ;
+    python experiments/applications/gaussian_process/benchmark_matvecs_versus_keops/benchmark_matvec.py --num_runs 3 --log_data_size $i --data_dim 4 ;
 done
