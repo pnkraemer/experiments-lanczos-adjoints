dc345547447e8be6259a1dbe8471916e2a1e275b
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-05 15:58:13 +0200

    Update the matvec benchmark

--- experiments/benchmark_gram_matvec_vs_keops/benchmark.py
+++ experiments/benchmark_gram_matvec_vs_keops/benchmark.py
@@ -15,8 +15,7 @@ MAX_BATCH = 2**30
 def k(x, y, p):
     """Evaluate a square-exponential kernel."""
     # maaaan, this is factor ~10 times faster than dot(x-y, x-y)
-    norm_sq = jnp.dot(x, x) + jnp.dot(y, y) - 2*jnp.dot(x, y)
-    
+    norm_sq = jnp.dot(x, x) + jnp.dot(y, y) - 2 * jnp.dot(x, y)
 
     return p[0] * jnp.exp(-p[1] * norm_sq)
 
@@ -29,7 +28,7 @@ def gram_map_checkpt(f, x, y, p, v, batch_size):
         fun_v = jax.vmap(f, in_axes=(None, 0, None), out_axes=-1)
         return jax.tree_util.tree_map(lambda s: jnp.dot(s, v), fun_v(z, y, p))
 
-    n, *shape = jnp.shape(x) 
+    n, *shape = jnp.shape(x)
     x_ = jnp.reshape(x, (n // batch_size, batch_size, *shape))
     mapped = jax.lax.map(jax.checkpoint(jax.vmap(fx)), x_)
     return jax.tree_util.tree_map(lambda s: s.reshape((-1,)), mapped)
@@ -43,7 +42,7 @@ def gram_map(f, x, y, p, v, batch_size):
         fun_v = jax.vmap(f, in_axes=(None, 0, None), out_axes=-1)
         return jax.tree_util.tree_map(lambda s: jnp.dot(s, v), fun_v(z, y, p))
 
-    n, *shape = jnp.shape(x) 
+    n, *shape = jnp.shape(x)
     x_ = jnp.reshape(x, (n // batch_size, batch_size, *shape))
     mapped = jax.lax.map(jax.vmap(fx), x_)
     return jax.tree_util.tree_map(
@@ -62,7 +61,9 @@ def gram_map_bwd(f, x, y, batch_size, cache, df):
     # beat one call (value_and_grad).
     # dv = gram_map(f, y, x, cache["p"], df, batch_size)
     # tmp = gram_map(jax.jacrev(f, argnums=2), y, x, cache["p"], df, batch_size)
-    dv, tmp = gram_map(jax.value_and_grad(f, argnums=2), y, x, cache["p"], df, batch_size)
+    dv, tmp = gram_map(
+        jax.value_and_grad(f, argnums=2), y, x, cache["p"], df, batch_size
+    )
     return cache["v"].T @ tmp, dv
 
 
@@ -70,11 +71,13 @@ def gradient(gram_fun, f, x, y, batch_size):
     """Evaluate the gradient of a vector-Gram-matrix-vector-product."""
 
     def loss(p, v):
-        key = jax.random.PRNGKey(1)
-        u = jax.random.normal(key, shape=v.shape, dtype=v.dtype)
-        return u @ gram_fun(f, x, y, p, v, batch_size)
+        return gram_fun(f, x, y, p, v, batch_size)
 
-    return jax.jit(jax.value_and_grad(loss, argnums=(0, 1)))
+    def full(p, v):
+        fwd, vjp = jax.vjp(loss, p, v)
+        return fwd, vjp(fwd)
+
+    return jax.jit(full)
 
 
 # Assigning the AD functions
@@ -84,7 +87,7 @@ gram_map_custom = gram_map
 gram_map_custom = jax.custom_vjp(gram_map_custom, nondiff_argnums=[0, 1, 2, 5])
 gram_map_custom.defvjp(gram_map_fwd, gram_map_bwd)
 
-Ns = 2 ** jnp.arange(15, 20, step=1, dtype=int)
+Ns = 2 ** jnp.arange(8, 14, step=1, dtype=int)
 ts_fwd = []
 ts_bwd_custom = []
 ts_bwd_checkpt = []
@@ -98,10 +101,10 @@ for N in Ns:
 
     # Create some test data
     X = jnp.linspace(0, 1, num=N)
-    X = jnp.stack([X]*dim, axis=1)
+    X = jnp.stack([X] * dim, axis=1)
     Y = X
     # Y = jnp.linspace(0, 1, num=N)
-    params = jnp.asarray([1.0, 1.])
+    params = jnp.asarray([1.0, 1.0])
     vector = jnp.linspace(0, 1, num=N)
 
     print("Benchmark the forward pass.", end=" ")
@@ -149,23 +152,23 @@ for N in Ns:
     ts_bwd_checkpt.append(t1)
     print(t1)
 
-    # print("Benchmark the forward+backward pass (custom).", end=" ")
-    # # Pre-compile
-    # fun = jax.jit(gradient(gram_map_custom, k, X, Y, batch_size=int(batch_size)))
+    print("Benchmark the forward+backward pass (custom).", end=" ")
+    # Pre-compile
+    fun = jax.jit(gradient(gram_map_custom, k, X, Y, batch_size=int(batch_size)))
 
-    # v0, (d0, d1) = fun(params, vector)
-    # v0.block_until_ready()
-    # d0.block_until_ready()
-    # d1.block_until_ready()
+    v0, (d0, d1) = fun(params, vector)
+    v0.block_until_ready()
+    d0.block_until_ready()
+    d1.block_until_ready()
 
-    # t0 = time.perf_counter()
-    # v0, (d0, d1) = fun(params, vector)
-    # v0.block_until_ready()
-    # d0.block_until_ready()
-    # d1.block_until_ready()
-    # t1 = time.perf_counter() - t0
-    # ts_bwd_custom.append(t1)
-    # print(t1)
+    t0 = time.perf_counter()
+    v0, (d0, d1) = fun(params, vector)
+    v0.block_until_ready()
+    d0.block_until_ready()
+    d1.block_until_ready()
+    t1 = time.perf_counter() - t0
+    ts_bwd_custom.append(t1)
+    print(t1)
 
 
 print("Saving to a file")
