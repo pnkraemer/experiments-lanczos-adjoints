3092e2c0d0f6839b4c4748777916586c194a554b
Peter Nicholas Kramer <pekra@dtu.dk>
2024-05-05 15:51:48 +0200

    Add some reference for GpyTorch to the playground

---
+++ experiments/playground/make_gpytorch_train_something.py
@@ -0,0 +1,89 @@
+import math
+import torch
+import gpytorch
+from matplotlib import pyplot as plt
+
+import urllib.request
+import os.path
+from scipy.io import loadmat
+from math import floor
+
+if not os.path.isfile('../3droad.mat'):
+    print('Downloading \'3droad\' UCI dataset...')
+    urllib.request.urlretrieve('https://www.dropbox.com/s/f6ow1i59oqx05pl/3droad.mat?dl=1', '../3droad.mat')
+
+data = torch.Tensor(loadmat('../3droad.mat')['data'])
+
+import numpy as np
+
+N = data.shape[0]
+# make train/val/test
+n_train = int(0.8 * N)
+train_x, train_y = data[:n_train, :-1], data[:n_train, -1]
+test_x, test_y = data[n_train:, :-1], data[n_train:, -1]
+
+# normalize features
+mean = train_x.mean(dim=-2, keepdim=True)
+std = train_x.std(dim=-2, keepdim=True) + 1e-6 # prevent dividing by 0
+train_x = (train_x - mean) / std
+test_x = (test_x - mean) / std
+
+# normalize labels
+mean, std = train_y.mean(),train_y.std()
+train_y = (train_y - mean) / std
+test_y = (test_y - mean) / std
+
+# make continguous
+train_x, train_y = train_x.contiguous(), train_y.contiguous()
+test_x, test_y = test_x.contiguous(), test_y.contiguous()
+
+output_device = torch.device('cuda:0')
+
+train_x, train_y = train_x.to(output_device), train_y.to(output_device)
+test_x, test_y = test_x.to(output_device), test_y.to(output_device)
+
+# We will use the simplest form of GP model, exact inference
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.keops.MaternKernel(nu=2.5))
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+# initialize likelihood and model
+likelihood = gpytorch.likelihoods.GaussianLikelihood().cuda()
+model = ExactGPModel(train_x, train_y, likelihood).cuda()
+
+# Find optimal model hyperparameters
+model.train()
+likelihood.train()
+
+# Use the adam optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+# "Loss" for GPs - the marginal log likelihood
+mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+import time
+training_iter = 50
+for i in range(training_iter):
+    start_time = time.time()
+    # Zero gradients from previous iteration
+    optimizer.zero_grad()
+    # Output from model
+    output = model(train_x)
+    # Calc loss and backprop gradients
+    loss = -mll(output, train_y)
+    loss.backward()
+    print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+        i + 1, training_iter, loss.item(),
+        model.covar_module.base_kernel.lengthscale.item(),
+        model.likelihood.noise.item()
+    ))
+    optimizer.step()
+    print(time.time() - start_time)
