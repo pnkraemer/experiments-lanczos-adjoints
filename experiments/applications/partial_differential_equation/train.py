a99509c706451ef8a4c6ed8a7dc600baaa857bdc
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-07 09:12:30 +0200

    Write training script

--- experiments/applications/partial_differential_equation/train.py
+++ experiments/applications/partial_differential_equation/train.py
@@ -1,6 +1,9 @@
 """Train an MLP on recovering the diffusion parameter."""
 
 import argparse
+import os
+import pickle
+import time
 
 import jax
 import jax.flatten_util
@@ -9,7 +12,7 @@ import jax.scipy.linalg
 import matplotlib.pyplot as plt
 import optax
 import tqdm
-from matfree_extensions.util import pde_util
+from matfree_extensions.util import exp_util, pde_util
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -22,21 +25,28 @@ args = parser.parse_args()
 print(args)
 
 
-# Load data
+print("\nLoading data...", end=" ")
 path = f"./data/pde_wave/{args.resolution}x{args.resolution}"
-
-inputs = jnp.load(f"{path}_data_inputs.npy")
-targets = jnp.load(f"{path}_data_targets.npy")
+inputs_all = jnp.load(f"{path}_data_inputs.npy")
+targets_all = jnp.load(f"{path}_data_targets.npy")
 parameter = jnp.load(f"{path}_data_parameter.npy")
+ndata = len(inputs_all)
+ntest = int(0.2 * ndata)
+print("done.")
+
+ntrain = ndata - ntest
+print(f"Splitting data into {ntrain}/{ntest} train/test points...", end=" ")
+test_inputs, test_targets = inputs_all[:ntest], targets_all[:ntest]
+inputs, targets = inputs_all[ntest:], targets_all[ntest:]
+print("done.")
 
 
-key = jax.random.PRNGKey(args.seed)
-
-# Create a mesh
+print("Creating a mesh...", end=" ")
 xs_1d = jnp.linspace(0.0, 1.0, endpoint=True, num=args.resolution)
 mesh = pde_util.mesh_tensorproduct(xs_1d, xs_1d)
+print("done.")
 
-# Discretise the PDE
+print("Discretising the PDE...", end=" ")
 dx_space = jnp.diff(xs_1d)[0]
 stencil = pde_util.stencil_laplacian(dx_space)
 boundary = pde_util.boundary_neumann()
@@ -50,53 +60,116 @@ def vector_field(x, p):
     return pde_rhs(scale=p)(x)
 
 
+print("done.")
+
+
+print(f"Setting up solver {args.method}...", end=" ")
 if args.method == "arnoldi":
     arnoldi_depth = 10
     expm = pde_util.expm_arnoldi(10)
     solve = pde_util.solver_expm(0.0, 1.0, vector_field, expm=expm)
 
+elif args.method == "diffrax:euler+backsolve":
+    method, adjoint = "euler", "backsolve"
+    kwargs = {"num_steps": 10, "method": method, "adjoint": adjoint}
+    solve = pde_util.solver_diffrax(0.0, 1.0, vector_field, **kwargs)
 
-mlp_features = [20, 20, 1]
-mlp_init, mlp_apply = pde_util.model_mlp(
-    mesh, mlp_features, activation=jnp.tanh, output_scale_raw=-5.0
-)
-key, subkey = jax.random.split(key, num=2)
-variables_before, mlp_unflatten = mlp_init(subkey)
-print(f"\nNumber of parameters: {variables_before.size}\n")
+elif args.method == "diffrax:heun+recursive_checkpoint":
+    method, adjoint = "heun", "recursive_checkpoint"
+    kwargs = {"num_steps": 10, "method": method, "adjoint": adjoint}
+    solve = pde_util.solver_diffrax(0.0, 1.0, vector_field, **kwargs)
+
+elif args.method == "diffrax:tsit5+recursive_checkpoint":
+    method, adjoint = "tsit5", "recursive_checkpoint"
+    kwargs = {"num_steps": 10, "method": method, "adjoint": adjoint}
+    solve = pde_util.solver_diffrax(0.0, 1.0, vector_field, **kwargs)
+
+elif args.method == "diffrax:dopri5+backsolve":
+    method, adjoint = "dopri5", "backsolve"
+    kwargs = {"num_steps": 10, "method": method, "adjoint": adjoint}
+    solve = pde_util.solver_diffrax(0.0, 1.0, vector_field, **kwargs)
+
+else:
+    msg = f"The method {args.method} is not supported."
+    raise ValueError(msg)
+print("done.")
+
+print("Setting up the MLP...", end=" ")
+key = jax.random.PRNGKey(args.seed)
+kwargs = {"output_scale_raw": -5.0, "activation": jnp.tanh}
+mlp_init, mlp_apply = pde_util.model_mlp(mesh, [20, 20, 1], **kwargs)
+variables_before, mlp_unflatten = mlp_init(key)
+print(f"with {variables_before.size} parameters.")
 
 
-# Create a loss function
+print("Creating a loss function...", end=" ")
 nugget = jnp.finfo(variables_before.dtype).eps
-loss = pde_util.loss_mse_relative(nugget=nugget, reduce=jnp.sum)
+error = pde_util.loss_mse_relative(nugget=nugget, reduce=jnp.sum)
 
 
 @jax.jit
 @jax.value_and_grad
 def loss_value_and_grad(p, y0s, y1s):
+    """Evaluate the loss over all input/output pairs."""
     scale = mlp_apply(mlp_unflatten(p), mesh)
     approx = jax.vmap(lambda *a: solve(*a)[0], in_axes=(0, None))(y0s, scale)
-    return loss(approx, targets=y1s)
+    return error(approx, targets=y1s)
 
 
+print("done.")
+
+
+print("Setting up an optimiser...", end=" ")
 variables_after = variables_before
 optimizer = optax.adam(args.learning_rate)
 opt_state = optimizer.init(variables_after)
+print("done.")
 
+print("Precompiling the value-and-grad...", end=" ")
+loss, grad = loss_value_and_grad(variables_after, inputs, targets)
+loss.block_until_ready()
+grad.block_until_ready()
+print("done.")
+
+print("Training...\n")
 progressbar = tqdm.tqdm(range(args.num_epochs))
 progressbar.set_description(f"Loss {0:.3e}")
+t0 = time.perf_counter()
 for _ in progressbar:
-    try:
-        loss, grad = loss_value_and_grad(variables_after, inputs, targets)
-        updates, opt_state = optimizer.update(grad, opt_state)
-        variables_after = optax.apply_updates(variables_after, updates)
-        progressbar.set_description(f"Loss {loss:.3e}")
-    except KeyboardInterrupt:
-        break
+    loss, grad = loss_value_and_grad(variables_after, inputs, targets)
+    updates, opt_state = optimizer.update(grad, opt_state)
+    variables_after = optax.apply_updates(variables_after, updates)
+    progressbar.set_description(f"Loss {loss:.3e}")
 
+t1 = time.perf_counter()
+scale_after = mlp_apply(mlp_unflatten(variables_after), mesh)
+
+print("\nEvaluating the error metrics...", end=" ")
+runtime = (t1 - t0) / args.num_epochs
+rmse = error(jnp.abs(scale_after), targets=jnp.abs(parameter))
+loss, _grad = loss_value_and_grad(variables_after, test_inputs, test_targets)
+print("\n\tRuntime per epoch:", runtime)
+print("\tRMSE (param):", rmse)
+print("\tLoss:", loss)
+print("done.\n")
+
+
+print("Saving results...", end=" ")
+directory = exp_util.matching_directory(__file__, "results/")
+os.makedirs(directory, exist_ok=True)
+path = f"{directory}{args.resolution}x{args.resolution}_{args.method}"
+stats = {"runtime": runtime, "rmse_param": rmse, "loss": loss}
+with open(f"{path}_stats.pkl", "wb") as handle:
+    pickle.dump(stats, handle)
+
+jnp.save(f"{path}_parameter.npy", scale_after)
+print("done.")
+
+print("Plotting results... todo: remove soon", end=" ")
 fig, axes = plt.subplots(ncols=2)
-
-img = axes[0].contourf(jnp.square(mlp_apply(mlp_unflatten(variables_after), mesh)))
+img = axes[0].contourf(jnp.abs(scale_after))
 plt.colorbar(img, ax=axes[0])
-img = axes[1].contourf(jnp.square(parameter))
+img = axes[1].contourf(jnp.abs(parameter))
 plt.colorbar(img, ax=axes[1])
 plt.show()
+print("done.")
