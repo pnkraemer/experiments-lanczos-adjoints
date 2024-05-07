d2882eea7fc88543ae612166815a020006ad1a11
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-07 19:29:14 +0200

    Plot the results (PDE)

--- experiments/applications/partial_differential_equation/plot.py
+++ experiments/applications/partial_differential_equation/plot.py
@@ -37,40 +37,51 @@ path = f"./data/pde_wave/{args.resolution}x{args.resolution}"
 parameter = jnp.load(f"{path}_data_parameter.npy")
 directory = exp_util.matching_directory(__file__, "results/")
 
+xs_1d = jnp.linspace(0, 1, endpoint=True, num=len(parameter))
+mesh = jnp.stack(jnp.meshgrid(xs_1d, xs_1d))
 
 layout = [
-    ["convergence", "truth", "arnoldi", "workprec"],
-    ["convergence", "dopri5", "tsit5", "workprec"],
+    ["convergence", "truth", "arnoldi", "workprec_fwd"],
+    ["convergence", "dopri5", "tsit5", "workprec_rev"],
 ]
 layout = onp.asarray(layout)
 
-fig, axes = plt.subplot_mosaic(layout, dpi=200, figsize=(8, 2), constrained_layout=True)
+fig, axes = plt.subplot_mosaic(layout, dpi=200, figsize=(8, 3), constrained_layout=True)
 
 
 axes["truth"].set_title("Truth", fontsize="small")
-img = axes["truth"].contourf(jnp.abs(parameter))
+img = axes["truth"].contourf(*mesh, jnp.abs(parameter), cmap="Greys")
 plt.colorbar(img, ax=axes["truth"])
 
-axes["arnoldi"].set_title("Arnoldi", fontsize="small")
+axes["arnoldi"].set_title(r"Solver: $\it{Arnoldi}$", fontsize="small")
 path = f"{directory}{args.resolution}x{args.resolution}_arnoldi"
 parameter_estimate = jnp.load(f"{path}_parameter.npy")
-img = axes["arnoldi"].contourf(jnp.abs(parameter_estimate))
+img = axes["arnoldi"].contourf(*mesh, jnp.abs(parameter_estimate), cmap="Blues")
 plt.colorbar(img, ax=axes["arnoldi"])
 
-axes["dopri5"].set_title("Dopri5", fontsize="small")
+axes["dopri5"].set_title("Solver: Dopri5", fontsize="small")
 path = f"{directory}{args.resolution}x{args.resolution}_diffrax:dopri5+backsolve"
 parameter_estimate = jnp.load(f"{path}_parameter.npy")
-img = axes["dopri5"].contourf(jnp.abs(parameter_estimate))
+img = axes["dopri5"].contourf(*mesh, jnp.abs(parameter_estimate), cmap="Greens")
 plt.colorbar(img, ax=axes["dopri5"])
 
-axes["tsit5"].set_title("Tsit5", fontsize="small")
+axes["tsit5"].set_title("Solver: Tsit5", fontsize="small")
 path = (
     f"{directory}{args.resolution}x{args.resolution}_diffrax:tsit5+recursive_checkpoint"
 )
 parameter_estimate = jnp.load(f"{path}_parameter.npy")
-img = axes["tsit5"].contourf(jnp.abs(parameter_estimate))
+img = axes["tsit5"].contourf(*mesh, jnp.abs(parameter_estimate), cmap="Greens")
 plt.colorbar(img, ax=axes["tsit5"])
 
+
+for method in ["tsit5", "arnoldi", "dopri5"]:
+    axes[method].sharex(axes["truth"])
+    axes[method].sharey(axes["truth"])
+
+    axes[method].set_xlim((0.0, 1.0))
+    axes[method].set_xticks((0.0, 0.5, 1.0))
+    axes[method].set_ylim((0.0, 1.0))
+    axes[method].set_yticks((0.0, 0.5, 1.0))
 
 axes["convergence"].set_title("Convergence", fontsize="small")
 axes["convergence"].set_xlabel("Time (sec)", fontsize="small")
@@ -80,22 +91,65 @@ for method in methods:
     parameter_estimate = jnp.load(f"{path}_parameter.npy")
     convergence = jnp.load(f"{path}_convergence.npy")
     timestamps = jnp.load(f"{path}_timestamps.npy")
-    axes["convergence"].semilogy(timestamps, convergence, label=labels[method])
-axes["convergence"].legend(fontsize="xx-small")
+    if "arnoldi" in method:
+        alpha, zorder = 0.9, 100
+    else:
+        alpha, zorder = 0.7, 0
+
+    axes["convergence"].semilogy(
+        timestamps, convergence, label=labels[method], zorder=zorder, alpha=alpha
+    )
+    axes["convergence"].legend(fontsize="x-small")
+    axes["convergence"].grid(axis="y", which="both")
 
 for method in methods:
     num_matvecs = jnp.load(f"{directory}/wp_{method}_Ns.npy")
     # ts_all = jnp.load(f"{directory}/wp_{method}_ts.npy")
-    # errors_fwd = jnp.load(f"{directory}/wp_{method}_errors_fwd.npy")
+    errors_fwd = jnp.load(f"{directory}/wp_{method}_errors_fwd.npy")
     errors_rev = jnp.load(f"{directory}/wp_{method}_errors_rev.npy")
     idx = num_matvecs >= 1
-    axes["workprec"].loglog(num_matvecs[idx], errors_rev[idx], label=labels[method])
+    if "arnoldi" in method:
+        alpha, zorder = 0.9, 100
+    else:
+        alpha, zorder = 0.7, 0
 
-axes["workprec"].set_xlabel("No. Matvecs", fontsize="small")
-axes["workprec"].set_ylabel("RMSE (relative)", fontsize="small")
-axes["workprec"].set_title("Gradient error", fontsize="small")
-axes["workprec"].legend(fontsize="xx-small")
-axes["workprec"].grid(which="major")
+    axes["workprec_fwd"].loglog(
+        num_matvecs[idx],
+        errors_fwd[idx],
+        label=labels[method],
+        alpha=alpha,
+        zorder=zorder,
+    )
+    axes["workprec_rev"].loglog(
+        num_matvecs[idx],
+        errors_rev[idx],
+        label=labels[method],
+        alpha=alpha,
+        zorder=zorder,
+    )
+
+
+axes["workprec_fwd"].sharex(axes["workprec_rev"])
+axes["workprec_fwd"].set_ylabel("RMSE (relative)", fontsize="small")
+axes["workprec_fwd"].set_title("Forward pass error", fontsize="small")
+axes["workprec_fwd"].legend(fontsize="xx-small")
+axes["workprec_fwd"].grid(which="major")
+
+axes["workprec_rev"].set_xlabel("No. Matvecs", fontsize="small")
+axes["workprec_rev"].set_ylabel("RMSE (relative)", fontsize="small")
+axes["workprec_rev"].set_title("Gradient error", fontsize="small")
+axes["workprec_rev"].legend(fontsize="xx-small")
+axes["workprec_rev"].grid(which="major")
+
+
+kwargs = dict(fontsize="small", fontweight="bold", loc="left")
+axes["convergence"].set_title("A.", **kwargs)
+axes["truth"].set_title("B1.", **kwargs)
+axes["arnoldi"].set_title("B2.", **kwargs)
+axes["dopri5"].set_title("B3.", **kwargs)
+axes["tsit5"].set_title("B4.", **kwargs)
+axes["workprec_fwd"].set_title("C1.", **kwargs)
+axes["workprec_rev"].set_title("C2.", **kwargs)
 
 directory = exp_util.matching_directory(__file__, "figures/")
 os.makedirs(directory, exist_ok=True)
