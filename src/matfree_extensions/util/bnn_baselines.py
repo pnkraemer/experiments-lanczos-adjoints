5aaa8a9dbb291cf591a556c55a647158c66349f3
h-roy <h-roy@github.com>
2024-05-08 13:33:19 +0200

    Last Layer Baseline

--- src/matfree_extensions/util/bnn_baselines.py
+++ src/matfree_extensions/util/bnn_baselines.py
@@ -165,3 +165,47 @@ def hutchinson_diagonal(
 
     diag, _ = jax.lax.fori_loop(0, num_levels, body_fun, (diag_init, key))
     return diag
+
+
+@partial(jax.jit, static_argnames=("likelihood", "model_fn"))
+def last_layer_ggn(
+        model_fn,
+        params,
+        x_batch,
+        likelihood: Literal["classification", "regression"] = "classification"
+):
+    leafs, _ = jax.tree_util.tree_flatten(params)
+    N_llla = len(leafs[-1]) + len(leafs[-2])
+    params_vec, unflatten_fn = jax.flatten_util.ravel_pytree(params)
+
+    def model_apply_vec(params_vectorized, x):
+        return model_fn(unflatten_fn(params_vectorized), x)
+    
+    def last_layer_model_fn(last_params_vec, first_params, x):
+        first_params = jax.lax.stop_gradient(first_params)
+        vectorized_params = jnp.concatenate([first_params, last_params_vec])
+        return model_apply_vec(vectorized_params, x)
+    
+    params_ll = params_vec[-N_llla:]
+    J_ll = jax.jacfwd(last_layer_model_fn, argnums=0)(params_ll, params_vec[:-N_llla], x_batch)
+    # B , O , N_llla
+    if likelihood == "regression":
+        J_ll = J_ll.reshape(-1, N_llla)
+        return J_ll.T @ J_ll
+    
+    elif likelihood == "classification":
+        pred = model_fn(params, x_batch) # B, O
+        pred = jax.nn.softmax(pred, axis=1)
+        pred = jax.lax.stop_gradient(pred)
+        D = jax.vmap(jnp.diag)(pred)
+        H = jnp.einsum("bo, bi->boi", pred, pred)
+        H = D - H # B, O, O
+        GGN_ll = jnp.einsum("mob, boo, bon->mn", J_ll.T, H, J_ll)
+        return GGN_ll
+
+
+
+
+
+
+
