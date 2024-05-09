3a0bec9aa5531a4e0c35d019869520f1370f35c1
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-09 09:50:53 +0200

    Complete partial Cholesky pivoting

--- tests/test_util/test_gp_util/test_cholesky_partial.py
+++ tests/test_util/test_gp_util/test_cholesky_partial.py
@@ -8,51 +8,134 @@ import pytest_cases
 from matfree import test_util
 
 
-def case_partial():
-    def cholesky_partial(rank):
-        def estimate(matrix):
-            L = jnp.zeros((len(matrix), rank))
+def case_cholesky_partial():
+    """Construct a partial Cholesky factorisation.
 
-            body = makebody(matrix)
-            return jax.lax.fori_loop(0, rank, body, L)
+    Return a function whose API looks like that of cholesky_partial_pivot,
+    so we only write a single test suite.
+    """
 
-        def makebody(matrix):
-            def body(i, L):
-                l_ii = jnp.sqrt(matrix[i, i] - jnp.dot(L[i], L[i]))
+    def cholesky_incomplete_pretend_pivoting(r):
+        def decompose(x):
+            alg = cholesky_partial(r)
+            return alg(x), jnp.arange(len(x))
 
-                l_ji = matrix[:, i] - L @ L[i, :]
-                l_ji /= l_ii
+        return decompose
 
-                return L.at[:, i].set(l_ji)
+    return cholesky_incomplete_pretend_pivoting
 
-            return body
 
-        return estimate
+def case_cholesky_partial_pivot():
+    """Construct a partial Cholesky factorisation with pivoting."""
+    return cholesky_partial_pivot
 
-    return cholesky_partial
+
+def cholesky_partial(rank):
+    """Construct a partial Cholesky factorisation."""
+
+    def estimate(matrix):
+        L = jnp.zeros((len(matrix), rank))
+
+        body = makebody(matrix)
+        return jax.lax.fori_loop(0, rank, body, L)
+
+    def makebody(matrix):
+        def body(i, L):
+            l_ii = jnp.sqrt(matrix[i, i] - jnp.dot(L[i], L[i]))
+
+            l_ji = matrix[:, i] - L @ L[i, :]
+            l_ji /= l_ii
+
+            return L.at[:, i].set(l_ji)
+
+        return body
+
+    return estimate
+
+
+def cholesky_partial_pivot(rank):
+    """Construct a partial Cholesky factorisation with pivoting."""
+
+    def estimate(matrix):
+        L = jnp.zeros((len(matrix), rank))
+        P = jnp.arange(len(matrix))
+        for i in range(rank):
+            # Find the largest entry for the residuals
+            residual = matrix - L @ L.T
+            res = jnp.abs(jnp.diag(residual))
+            k = jnp.argmax(res)
+
+            # Pivot (pivot!!! pivot!!! pivot!!!)
+            matrix = _swap_rows(matrix, i, k)
+            matrix = _swap_cols(matrix, i, k)
+            L = _swap_rows(L, i, k)
+            P = _swap_rows(P, i, k)
+
+            # Perform a Cholesky step
+            l_ii = jnp.sqrt(matrix[i, i] - jnp.dot(L[i], L[i]))
+            l_ji = matrix[:, i] - L @ L[i, :]
+            l_ji /= l_ii
+
+            # Update the estimate
+            L = L.at[:, i].set(l_ji)
+
+        return L, P
+
+    return estimate
+
+
+def _swap_cols(arr, i, j):
+    return _swap_rows(arr.T, i, j).T
+
+
+def _swap_rows(arr, i, j):
+    ai, aj = arr[i], arr[j]
+    arr = arr.at[i].set(aj)
+    return arr.at[j].set(ai)
+
+
+def pivot_apply_inverse(arr, pivot, /):
+    """Invert and apply a pivoting array to a matrix."""
+    return arr[jnp.argsort(pivot)]
 
 
 @pytest_cases.parametrize_with_cases("chol", ".")
-def test_full(chol: Callable, n=5):
-    key = jax.random.PRNGKey(1)
+def test_full_rank_partial_cholesky_matches_full_cholesky(chol, n=5):
+    key = jax.random.PRNGKey(2)
 
-    cov_eig = 0.1 + jax.random.uniform(key, shape=(n,))
+    cov_eig = 1.0 + jax.random.uniform(key, shape=(n,), dtype=float)
     cov = test_util.symmetric_matrix_from_eigenvalues(cov_eig)
-    reference = jnp.linalg.cholesky(cov)
 
-    approximation = chol(n)(cov)
+    approximation, piv = chol(n)(cov)
+    approximation_p = pivot_apply_inverse(approximation, piv)
 
     tol = jnp.finfo(approximation.dtype).eps
-    assert jnp.allclose(approximation, reference, atol=tol, rtol=tol)
+    assert jnp.allclose(approximation_p @ approximation_p.T, cov, atol=tol, rtol=tol)
 
 
 @pytest_cases.parametrize_with_cases("chol", ".")
-def test_partial(chol: Callable, n=4, rank=2):
+def test_output_the_right_shapes(chol: Callable, n=4, rank=2):
     key = jax.random.PRNGKey(1)
 
     cov_eig = 0.1 + jax.random.uniform(key, shape=(n,))
     cov = test_util.symmetric_matrix_from_eigenvalues(cov_eig)
 
-    approximation = chol(rank)(cov)
+    approximation, piv = chol(rank)(cov)
 
     assert approximation.shape == (n, rank)
+    assert piv.shape == (n,)
+
+
+def test_pivoting_improves_the_estimate(n=10, rank=5):
+    key = jax.random.PRNGKey(1)
+
+    cov_eig = 0.1 + jax.random.uniform(key, shape=(n,))
+    cov = test_util.symmetric_matrix_from_eigenvalues(cov_eig)
+
+    nopivot = cholesky_partial(rank)(cov)
+    pivot, p = cholesky_partial_pivot(rank)(cov)
+    pivot = pivot_apply_inverse(pivot, p)
+
+    error_nopivot = jnp.linalg.norm(cov - nopivot @ nopivot.T)
+    error_pivot = jnp.linalg.norm(cov - pivot @ pivot.T)
+    assert error_pivot < error_nopivot
