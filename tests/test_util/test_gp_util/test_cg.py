cc4cc54126d7e342bed3c6f34155ec53b01a287f
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-10 11:45:22 +0200

    CG takes the number of matvecs as an argument

--- tests/test_util/test_gp_util/test_cg.py
+++ tests/test_util/test_gp_util/test_cg.py
@@ -12,28 +12,32 @@ def test_sth():
     b = jnp.arange(1.0, 10.0)
     solution = jnp.linalg.solve(A, b)
 
-    approximation = _cg(lambda v: A @ v, b, jnp.zeros_like(b))
+    solve = cg_fixed(lambda v: A @ v, len(A))
+    approximation = solve(b, jnp.zeros_like(b))
     assert jnp.allclose(approximation, solution)
 
 
-def _cg(A: Callable, b, x):
-    return _pcg(A, b, x, lambda v: v)
+def cg_fixed(A: Callable, num_matvecs: int, /):
+    return pcg_fixed(A, num_matvecs, lambda v: v)
 
 
-def _pcg(A: Callable, b, x, M: Callable):
-    r = b - A(x)
-    z = M(r)
-    p = z
-    for _ in range(len(x)):
-        Ap = A(p)
-        a = jnp.dot(r, z) / (p.T @ Ap)
-        x = x + a * p
-
-        rold = r
-        r = r - a * Ap
-
-        zold = z
+def pcg_fixed(A: Callable, num_matvecs: int, M: Callable, /):
+    def pcg(b, x):
+        r = b - A(x)
         z = M(r)
-        b = jnp.dot(r, z) / jnp.dot(rold, zold)
-        p = z + b * p
-    return x
+        p = z
+        for _ in range(num_matvecs):
+            Ap = A(p)
+            a = jnp.dot(r, z) / (p.T @ Ap)
+            x = x + a * p
+
+            rold = r
+            r = r - a * Ap
+
+            zold = z
+            z = M(r)
+            b = jnp.dot(r, z) / jnp.dot(rold, zold)
+            p = z + b * p
+        return x
+
+    return pcg
