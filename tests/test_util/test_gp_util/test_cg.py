d6d0b5d9c57d4a2a611c2e50254a5f9fef1a1298
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-10 11:42:47 +0200

    CG and PCG are matrix-free

--- tests/test_util/test_gp_util/test_cg.py
+++ tests/test_util/test_gp_util/test_cg.py
@@ -1,5 +1,7 @@
 """Tests for conjugate gradients."""
 
+from typing import Callable
+
 import jax.numpy as jnp
 from matfree import test_util
 
@@ -10,27 +12,28 @@ def test_sth():
     b = jnp.arange(1.0, 10.0)
     solution = jnp.linalg.solve(A, b)
 
-    approximation = _cg(A, b, jnp.zeros_like(b))
+    approximation = _cg(lambda v: A @ v, b, jnp.zeros_like(b))
     assert jnp.allclose(approximation, solution)
 
 
-def _cg(A, b, x):
-    return _pcg(A, b, x, jnp.eye(len(A)))
+def _cg(A: Callable, b, x):
+    return _pcg(A, b, x, lambda v: v)
 
 
-def _pcg(A, b, x, M):
-    r = b - A @ x
-    z = M @ r
+def _pcg(A: Callable, b, x, M: Callable):
+    r = b - A(x)
+    z = M(r)
     p = z
-    for _ in range(len(A)):
-        a = jnp.dot(r, z) / (p.T @ A @ p)
+    for _ in range(len(x)):
+        Ap = A(p)
+        a = jnp.dot(r, z) / (p.T @ Ap)
         x = x + a * p
 
         rold = r
-        r = r - a * A @ p
+        r = r - a * Ap
 
         zold = z
-        z = M @ r
+        z = M(r)
         b = jnp.dot(r, z) / jnp.dot(rold, zold)
         p = z + b * p
     return x
