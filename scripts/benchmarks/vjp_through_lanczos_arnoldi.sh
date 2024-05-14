92b06bd560654a076893116228a641d379b149c0
Peter Nicholas Kramer <pekra@dtu.dk>
2024-05-14 08:00:31 +0200

    Prepare the last VJP runtime run

--- scripts/benchmarks/vjp_through_lanczos_arnoldi.sh
+++ scripts/benchmarks/vjp_through_lanczos_arnoldi.sh
@@ -1,19 +1,38 @@
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho full --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho full --num_runs 5 --precompile
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho none --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho none --num_runs 5 --precompile
+#!/bin/bash
 
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho full --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho full --num_runs 5 --precompile
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho none --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "1138_bus" --max_krylov_depth 500 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho none --num_runs 5 --precompile
+#BSUB -q gpuv100
+#BSUB -J runtime-run
+#BSUB -n 8
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 6:00
+#BSUB -R "select[gpu32gb]"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -o logs/runtime-run-%J.out
+#BSUB -e logs/runtime-run-%J.err
 
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho full --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho full --num_runs 5 --precompile
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho none --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi arnoldi --reortho none --num_runs 5 --precompile
+### Load the cuda module
+module load cuda/12.4
+source ../penv/bin/activate
 
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho full --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho full --num_runs 5 --precompile
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho none --num_runs 1
-python experiments/benchmark_vjp_wall_times/suite_sparse/benchmark.py  --which_matrix "gyro" --max_krylov_depth 1000 --backprop_until 100 --lanczos_or_arnoldi lanczos --reortho none --num_runs 5 --precompile
+# Stop backprop early (bc inefficient)
+backprop_until=100
+
+# Go up to K=500
+max_krylov_depth=50
+
+# Test different matrices, methods, reorthogonalisation
+# and measure compile- and run-time
+
+for matrix in "1138_bus" "gyro"
+do
+    for method in "arnoldi" "lanczos" 
+    do
+        for reortho in "full" "none" 
+        do 
+            time python experiments/benchmarks/wall_times_vjp_through_lanczos_arnoldi/suite_sparse/benchmark.py  --which_matrix $matrix --lanczos_or_arnoldi $method --reortho $reortho --max_krylov_depth $max_krylov_depth --backprop_until $backprop_until  --num_runs 1;
+
+            time python experiments/benchmarks/wall_times_vjp_through_lanczos_arnoldi/suite_sparse/benchmark.py --which_matrix $matrix --lanczos_or_arnoldi $method --reortho $reortho --max_krylov_depth $max_krylov_depth --backprop_until $backprop_until  --num_runs 5 --precompile;
+        done 
+    done
+done
